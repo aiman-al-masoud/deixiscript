6780ec9027473403b34e12eb85920b3a3ad9eaae
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-10 00:06:16 +0100

    lexer interface

--- app/src/lexer/Lexer.ts
+++ app/src/lexer/Lexer.ts
@@ -1,3 +1,8 @@
+import Token from "../ast/interfaces/Token"
+
 export default interface Lexer{
-    
+    next():void
+    backTo(pos:number):void
+    peek():Token
+    croak(errorMsg:string):void   
 }
