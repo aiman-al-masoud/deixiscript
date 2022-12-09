b1a1cde0ad384156251ba776c1674cb0fded971a
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-10 00:28:35 +0100

    ...

--- app/src/lexer/tokenOf.ts
+++ app/src/lexer/tokenOf.ts
@@ -1,6 +1,15 @@
+import articles from '../../res/articles'
+import copulas from '../../res/copulas'
+import hverbs from '../../res/hverbs'
 import iverbs from '../../res/iverbs'
 import mverbs from '../../res/mverbs'
-
+import negations from '../../res/negations'
+import nonsubconj from '../../res/nonsubconj'
+import prepositions from '../../res/prepositions'
+import quantifiers from '../../res/quantifiers'
+import relprons from '../../res/relprons'
+import subconj from '../../res/subconj'
+import then from '../../res/then'
 
 export default function tokenOf(string:string){
     
