5432355c0428512fd4cfab9dce1648f701b53601
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-10 11:56:09 +0100

    Basic Parser skeleton

---
+++ app/src/parser/BasicParser.ts
@@ -0,0 +1,105 @@
+import Ast from "../ast/interfaces/Ast";
+import BinaryQuestion from "../ast/interfaces/BinaryQuestion";
+import CompoundSentence from "../ast/interfaces/CompoundSentence";
+import Declaration from "../ast/interfaces/Declaration";
+import Question from "../ast/interfaces/Question";
+import SimpleSentence from "../ast/interfaces/SimpleSentence";
+import VerbSentence from "../ast/interfaces/VerbSentence";
+import Complement from "../ast/phrases/Complement";
+import NounPhrase from "../ast/phrases/NounPhrase";
+import SubordinateClause from "../ast/phrases/SubordinateClause";
+import ComplexSentence from "../ast/sentences/ComplexSentence";
+import ConjunctiveSentence from "../ast/sentences/ConjunctiveSentence";
+import CopulaQuestion from "../ast/sentences/CopulaQuestion";
+import CopulaSentence from "../ast/sentences/CopulaSentence";
+import IntransitiveSentence from "../ast/sentences/IntransitiveSentence";
+import MonotransitiveSentence from "../ast/sentences/MonotransitiveSentence";
+import Copula from "../ast/tokens/Copula";
+import Lexer, { getLexer } from "../lexer/Lexer";
+import Parser from "./Parser";
+
+export default class BasicParser implements Parser{
+
+    private lx:Lexer
+
+    constructor(sourceCode:string){
+        this.lx = getLexer(sourceCode)
+    }
+    
+    parse(): Ast {
+        const memento = this.lx.pos
+        try{ return this.parseDeclaration() }catch{}
+        this.lx.backTo(memento)
+        return this.parseQuestion()
+    }
+
+    protected parseDeclaration():Declaration{
+
+    }
+
+    protected parseQuestion():Question{
+
+    }
+
+    protected parseSimple():SimpleSentence{
+
+    }
+
+    protected parseCompound():CompoundSentence{
+
+    }
+
+    protected parseVerbSentence():VerbSentence{
+
+    }
+
+    protected parseCopulaSentence():CopulaSentence{
+
+    }
+
+    protected parseComplex():ComplexSentence{
+
+    }
+
+    protected parseConjunctive():ConjunctiveSentence{
+
+    }
+
+    protected parseCopulaSentence():CopulaSentence{
+
+    }
+
+    protected parseVerbSentence():VerbSentence{
+
+    }
+
+    protected parseIntransitiveSentence():IntransitiveSentence{
+
+    }
+
+    protected parseMonotransitiveSentence():MonotransitiveSentence{
+
+    }
+
+    protected parseBinaryQuestion():BinaryQuestion{
+
+    }
+
+    protected parseCopulaQuestion():CopulaQuestion{
+
+    }
+
+    protected parseNounPhrase():NounPhrase{
+
+    }
+
+    protected parseComplement():Complement{
+
+    }
+
+    protected parseSubordinateClause():SubordinateClause{
+
+    }
+
+    
+}
--- app/src/parser/Parser.ts
+++ app/src/parser/Parser.ts
@@ -1,3 +1,10 @@
+import Ast from "../ast/interfaces/Ast";
+import BasicParser from "./BasicParser";
+
 export default interface Parser{
-       
+    parse():Ast   
+}
+
+export function getParser(sourceCode:string){
+    return new BasicParser(sourceCode)
 }
