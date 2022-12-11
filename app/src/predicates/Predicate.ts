707e707a06b7b452c4fdb29683d18dc55d0fb31f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-11 13:57:22 +0100

    ...

--- app/src/predicates/Predicate.ts
+++ app/src/predicates/Predicate.ts
@@ -2,7 +2,7 @@ export default interface Predicate{
     verb:string 
     method:string
     classes:string[]
-    parameters:{// verb preposition arg to method param
+    parameters:{// verb arguments (including subject, object and complements) mapped to method param
         string:string
     }
     apply(object:any, ...args:any[]):any
