3e573c0e42a7f531f122eebcc5a1a1cfd69ecb7a
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-11 15:02:15 +0100

    predicates

--- app/src/predicates/Predicate.ts
+++ app/src/predicates/Predicate.ts
@@ -2,9 +2,30 @@ export default interface Predicate{
     verb:string 
     method:string
     classes:string[]
-    parameters:{// verb arguments (including subject, object and complements) mapped to method param
-        string:string
-    }
-    apply(object:any, ...args:any[]):any
+    paramsMap:ParamsMap
+    apply(args:Args):any
+}
+
+export interface Args{
+    subject:any
+    object?:any
+    on?:any
+    with?:any
+    for?:any
+    by?:any
+    string:any
+}
+
+/**
+ * Maps a verb's params to a method's params
+ */
+export interface ParamsMap{ // 0-> object, 1-> first method param, 2-> second method param ... 
+    subject:number
+    object?:number
+    on?:number
+    with?:number
+    for?:number
+    by?:number
+    string:number
 }
 
