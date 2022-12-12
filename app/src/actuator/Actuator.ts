cde8a54f36df4337daf3bcd02fea4006133d68f5
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-12 10:52:42 +0100

    Revert "updated arch"
    This reverts commit 9ccbab579ae131b83363602684eb4ad6a251ad00.

--- app/src/actuator/Actuator.ts
+++
@@ -1,5 +0,0 @@
-import Predicate from "../predicates/Predicate";
-
-export default interface Actuator{
-    onStateChange(predicates:Predicate[]):void
-}
--- app/src/universe/Universe.ts
+++ app/src/universe/Universe.ts
@@ -1,12 +1,6 @@
-import Brain from "./Brain"
-
-/**
- * Acts as a gateway to the actual brain
- */
-export default interface Universe extends Brain{
+export default interface Universe{
     find(query:string):any[]
     check(query:string):boolean
     assume(code:string):void
     clone():Universe
-    addListener(object:Element, event:string, handler:(event:Event)=>{}):void
 }
