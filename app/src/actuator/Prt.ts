1fbfe3bb67b55092b08aa6a6c373274823b0a8f8
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-15 12:56:05 +0100

    ...

--- app/src/actuator/Prt.ts
+++ app/src/actuator/Prt.ts
@@ -4,6 +4,6 @@ import Predicate from "../predicates/Predicate";
  * Predicate Resultion Table
  */
 export default interface Prt{
-    get(verb:string, object:any):Predicate
+    get(verbOrAdj:string, object:any):Predicate
     getIncompatible(verb:string, object:any):Predicate[]
 }
