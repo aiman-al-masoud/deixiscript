a691513268bd586f3f76873cf37d52f34de18f10
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-15 11:28:45 +0100

    Actuator Action

--- app/src/actuator/Actuator.ts
+++ app/src/actuator/Actuator.ts
@@ -1,5 +1,20 @@
-import Predicate from "../predicates/Predicate";
+export default interface Actuator {
+    onStateChanged(...actions: Action[]): void
+}
 
-export default interface Actuator{
-    onStateChanged(predicates:Predicate[]):void
+export interface Action {
+    verb: string
+    args: ActionArgs
+}
+
+type Id = number | string
+
+export interface ActionArgs {
+    subject?: Id
+    object?: Id
+    on?: Id
+    with?: Id
+    for?: Id
+    by?: Id
+    to?: Id
 }
