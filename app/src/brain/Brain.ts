0484c99afae17479969d0dae51d2cb84c65eebf9
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-18 20:15:06 +0100

    sanbox interface only

--- app/src/brain/Brain.ts
+++ app/src/brain/Brain.ts
@@ -1,8 +1,10 @@
+import { Id } from "../clauses/Clause"
+
 export default interface Brain{
-    find(query:string):number[]
+    find(query:string):Id[]
     check(query:string):boolean
     assume(code:string):void
-    clone():Brain
     
-    addListener(element:number, event:string, callback:(event:Event)=>void):void
+    // clone():Brain
+    // addListener(element:number, event:string, callback:(event:Event)=>void):void
 }
---
+++ app/src/brain/Sandbox.ts
@@ -0,0 +1,44 @@
+import { Clause, Id } from "../clauses/Clause";
+import Brain from "./Brain";
+
+/**
+ * Mapping any given id in the sandbox to an id in the 
+ * larger universe.
+ */
+export type Map = { [a: Id]: Id }
+
+/**
+ * The {@link Brain} should call {@link Sandbox.mapTo()} before asserting the content of a 
+ * new {@link Clause}. This is to get a {@link Map}ping between entity {@link Id}s in 
+ * sandbox and Brain, to resolve anaphora and avoid creating duplicate entities.
+ */
+export interface Sandbox {
+    mapTo(universe: Brain): Map
+}
+
+export function getSandbox(clause: Clause) {
+    return new BaseSandbox(clause)
+}
+
+class BaseSandbox implements Sandbox {
+
+    constructor(readonly clause: Clause) {
+
+        // create a prolog "sandbox"
+
+        // write all contents of clause to the sandbox
+        
+    }
+
+    mapTo(universe: Brain): Map {
+        
+        // get entities in sandbox
+
+        // map entity in sanbox to its full description 
+
+        // map full description to corresponding id in universe, if any
+
+        throw new Error("Method not implemented.");
+    }
+
+}
