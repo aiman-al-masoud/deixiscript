812aee056ccc36a670609042c6ea6a4a9b8ea428
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-20 16:47:46 +0100

    comment

--- app/src/brain/Sandbox.ts
+++ app/src/brain/Sandbox.ts
@@ -2,9 +2,8 @@ import { Clause, Id, Map } from "../clauses/Clause";
 import Brain from "./Brain";
 
 /**
- * The {@link Brain} should call {@link Sandbox.mapTo()} before asserting the content of a 
- * new {@link Clause}. This is to get a {@link Map}ping between entity {@link Id}s in 
- * sandbox and Brain, to resolve anaphora and avoid creating duplicate entities.
+ * Entities in a new sentence potentially point to existing entities
+ * in the universe (ie: {@link Brain}). {@link Sandbox} resolves the anaphora.
  */
 export interface Sandbox {
     mapTo(universe: Brain): Map
@@ -17,7 +16,7 @@ export function getSandbox(clause: Clause) {
 class BaseSandbox implements Sandbox {
 
     constructor(readonly clause: Clause) {
-        
+
     }
 
     mapTo(universe: Brain): Map {
