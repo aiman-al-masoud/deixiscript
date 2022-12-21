aeec152d87cdb78b4fd72a213784eeafc7b59fbf
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-12-21 11:14:57 +0100

    modified brain interface

--- app/src/brain/Brain.ts
+++ app/src/brain/Brain.ts
@@ -1,9 +1,10 @@
 import { Clause, Id } from "../clauses/Clause"
 
 export default interface Brain{
-    find(query:Clause):Promise<Id[]>
-    check(query:Clause):Promise<boolean>
-    assume(code:Clause):Promise<void>
+    // find(query:Clause):Promise<Id[]>
+    // check(query:Clause):Promise<boolean>
+    query(query:Clause):Promise<Id[] | boolean>
+    assert(code:Clause):Promise<void>
     
     // clone():Brain
     // addListener(element:number, event:string, callback:(event:Event)=>void):void
