a7b7e91d3a5126adf252b71ed17d9c12af868a63
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-05 12:58:47 +0100

    ...

--- app/src/concepts/get.ts
+++ app/src/concepts/get.ts
@@ -1,16 +1,7 @@
-import { getConcepts, getGetterName, getIsName } from "./getConcepts"
+import { getGetterName } from "./getConcepts"
 
 
 export function get(object: any, concept: string) {
-
-    // const objectConcepts = getConcepts(object)
-    // const propConcepts = getConcepts(prop)
-
-    // const matchingConcepts = propConcepts
-    //     .filter(x => objectConcepts.includes(x))
-
-    // return matchingConcepts
-    //     .some(x => object[getIsName(x)].bind(object)(prop))
 
     return object[getGetterName(concept)].bind(object)()
 }
