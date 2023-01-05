7e9eecfb6b96615e72119384e293a591b6fe1ef7
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-06 00:39:05 +0100

    ...

--- app/src/brain/BasicBrain.ts
+++ app/src/brain/BasicBrain.ts
@@ -1,5 +1,6 @@
-import { Clause } from "../clauses/Clause";
-import { Map } from "../clauses/Id";
+import { BasicClause } from "../clauses/BasicClause";
+import { Clause, clauseOf } from "../clauses/Clause";
+import { Id, Map } from "../clauses/Id";
 import { getParser } from "../parser/Parser";
 import Brain, { AssertOpts } from "./Brain";
 import getEnviro from "./Enviro";
@@ -14,7 +15,38 @@ export default class BasicBrain implements Brain {
 
         for (const ast of getParser(natlang).parseAll()) {
             const clause = await ast.toClause()
+
             console.log(clause, clause.toString(), 'side-effetcs:', clause.isSideEffecty)
+
+
+            const topLevelEntities =
+                clause.
+                    entities
+                    .map(e => ({ e, c: clause.about(e) }))
+                    .filter(x => !x.c.flatList().some(i =>
+                        (i as BasicClause).predicate === 'of'
+                        && (i as BasicClause).args[0] === x.e))
+                    .map(x => x.e)
+
+            const ownedBy = (owner: Id) =>
+                clause
+                    .entities
+                    .map(e => ({ e, c: clause.about(e) }))
+                    .filter(x => x.c.flatList().some(i =>
+                        (i as BasicClause).predicate === 'of'
+                        && (i as BasicClause).args[0] === x.e
+                        && (i as BasicClause).args[1] === owner
+                    ))
+                    .map(x => x.e)
+
+            const secondLevelEntities = ownedBy(topLevelEntities[0])
+            const thridLevelEntities = ownedBy(secondLevelEntities[0])
+
+            console.log({ topLevelEntities })
+            console.log({ secondLevelEntities })
+            console.log({ thridLevelEntities })
+
+
         }
 
         return []
