f33f5cce6e241dab4bff0bd54108ab3a9d53c8e5
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-06 01:42:11 +0100

    ...

--- app/src/concepts/Wrapper.ts
+++ app/src/concepts/Wrapper.ts
@@ -1,3 +1,6 @@
+import { Clause } from "../clauses/Clause"
+import { Id } from "../clauses/Id"
+
 export default interface Wrapper {
 
     set(predicate: string, ...args: Wrapper[]): void // obj.set('red'), obj.set('on', obj2) ...
@@ -8,5 +11,6 @@ export default interface Wrapper {
     isAll(): string[] // ['button', 'red', 'big', ...]
     setAlias(name: string, path: string[]): void // .setAlias('width', ['style', 'width'])
     // addConcept(concept:string, setter:()=>void, is:()=>):void
+    // doSomething(clause:Clause):any // get ownership chain and do something with the clause, clause has everything, it has info on side-effects, predicate etc...???
 
 }
