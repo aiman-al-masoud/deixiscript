0acab46052d2723875f888084fc5dabec4ca1399
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-06 21:08:25 +0100

    ...

--- app/src/concepts/Wrapper.ts
+++ app/src/concepts/Wrapper.ts
@@ -8,7 +8,7 @@ export default interface Wrapper {
     get(predicate: string): any
     getProp(path: string[]): any
     setProp(path: string[], value: any): void
-    isAll(): string[] // ['button', 'red', 'big', ...]
+    describe(): string[] // ['button', 'red', 'big', ...]
     setAlias(name: string, path: string[]): void // .setAlias('width', ['style', 'width'])
     // addConcept(concept:string, setter:()=>void, is:()=>):void
     // doSomething(clause:Clause):any // get ownership chain and do something with the clause, clause has everything, it has info on side-effects, predicate etc...???
