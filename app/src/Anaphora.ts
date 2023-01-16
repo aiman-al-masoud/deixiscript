900e8ec3b3572769ead360ceeb723f97d51b60c2
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-16 14:40:03 +0100

    ...

---
+++ app/src/Anaphora.ts
@@ -0,0 +1,47 @@
+import { BasicClause } from "./clauses/BasicClause";
+import { Clause } from "./clauses/Clause";
+import { getRandomId, Map } from "./clauses/Id"
+import getEnviro from "./enviro/Enviro";
+
+export interface Anaphora {
+    assert(clause: Clause): Promise<void>
+    query(clause: Clause): Promise<Map[]>
+}
+
+export function getAnaphora() {
+    return new EnviroAnaphora()
+}
+
+class EnviroAnaphora implements Anaphora {
+
+    constructor(protected readonly enviro = getEnviro()) {
+
+    }
+
+    async assert(clause: Clause): Promise<void> {
+
+        const clauses = clause
+            .flatList()
+            .map(c => c as BasicClause)
+
+        for (const c of clauses) {
+
+            if (c.args.length == 1) {
+
+                this.enviro.setPlaceholder(c.args[0])
+                const x = await this.enviro.get(c.args[0])
+                console.log(c.args[0], ' is a ', c.predicate)
+                x.set(c.predicate)
+
+            }
+
+        }
+
+    }
+
+    async query(clause: Clause): Promise<Map[]> {
+        return [await this.enviro.query(clause)]
+    }
+
+}
+
---
+++ app/src/tests/anaphoratest.ts
@@ -0,0 +1,12 @@
+import { getAnaphora } from "../Anaphora";
+import { clauseOf } from "../clauses/Clause";
+import { getParser } from "../parser/Parser";
+
+export async function anaphoratest() {
+
+    const c = await getParser('the red button is a button').parse().toClause()
+    const a = getAnaphora()
+    await a.assert(c)
+    const r = await a.query(await getParser('the red button').parse().toClause() )
+    console.log(r)
+}
