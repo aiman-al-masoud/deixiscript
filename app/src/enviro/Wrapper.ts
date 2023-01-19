e1df77c272950ce6e2548141ce116b7725f9d311
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-19 10:31:40 +0100

    rm useless comments

--- app/src/enviro/Wrapper.ts
+++ app/src/enviro/Wrapper.ts
@@ -2,21 +2,14 @@ import ConcreteWrapper from "./ConcreteWrapper"
 
 export default interface Wrapper {
 
-    set(predicate: string, props?: string[]): void // obj.set('red'), obj.set('on', obj2) ...
+    readonly object: any
+    set(predicate: string, props?: string[]): void
     is(predicate: string, ...args: Wrapper[]): boolean
     setAlias(conceptName: string, propPath: string[]): void
-    readonly object: any
     pointOut(opts?: { turnOff: boolean }): void
     // get(predicate: string): any
-    // getProp(path: string[]): any
-    // setProp(path: string[], value: any): void
-    // describe(): string[] // ['button', 'red', 'big', ...]
-    // setAlias(name: string, path: string[]): void // .setAlias('width', ['style', 'width'])
-    // addConcept(concept:string, setter:()=>void, is:()=>):void
-    // doSomething(clause:Clause):any // get ownership chain and do something with the clause, clause has everything, it has info on side-effects, predicate etc...???
 
 }
-
 
 export function wrap(o: any) {
     return new ConcreteWrapper(o)
