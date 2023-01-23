268d9bc9359a397c1a8cf5823db6077f96d61e35
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-23 12:38:16 +0100

    ...

--- app/src/actuator/ImplyAction.ts
+++ app/src/actuator/ImplyAction.ts
@@ -44,7 +44,7 @@ export default class ImplyAction implements Action {
         const top = this.condition.topLevel()[0]
         const protoName = this.condition.describe(top)[0] // assume one 
         const predicate = this.conclusion.describe(top)[0]
-        const y = enviro.query(clauseOf({ type: 'noun', root: protoName.root }, 'X'))
+        const y = enviro.query(clauseOf(protoName, 'X'))
         const ids = y.map(m => m['X'])
         ids.forEach(id => new Edit(id, predicate).run(enviro))
     }
