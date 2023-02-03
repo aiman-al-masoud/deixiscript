fc4713115f87358254064c328cc8743a75cf1661
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-02-03 19:32:11 +0100

    rm useless parens

--- app/src/tests/autotester.ts
+++ app/src/tests/autotester.ts
@@ -33,8 +33,8 @@ export default async function autotester() {
 function test1() {
     const brain = getBrain({ root: document.body })
     brain.execute('x is red. x is a button. y is a green button.');
-    const assert1 = (brain.execute('a green button'))[0].style.background === 'green'
-    const assert2 = (brain.execute('a red button'))[0].style.background === 'red'
+    const assert1 = brain.execute('a green button')[0].style.background === 'green'
+    const assert2 = brain.execute('a red button')[0].style.background === 'red'
     return assert1 && assert2
 }
 
@@ -48,9 +48,9 @@ function test2() {
 function test3() {
     const brain = getBrain({ root: document.body })
     brain.execute('y is a button. x is red. y is a green button. x is a button. z is a black button.');
-    const assert1 = (brain.execute('a red button'))[0].style.background === 'red'
-    const assert2 = (brain.execute('a green button'))[0].style.background === 'green'
-    const assert3 = (brain.execute('a black button'))[0].style.background === 'black'
+    const assert1 = brain.execute('a red button')[0].style.background === 'red'
+    const assert2 = brain.execute('a green button')[0].style.background === 'green'
+    const assert3 = brain.execute('a black button')[0].style.background === 'black'
     return assert1 && assert2 && assert3
 }
 
@@ -64,14 +64,14 @@ function test4() {
 function test5() {
     const brain = getBrain({ root: document.body })
     brain.execute('x is a button. the color of x is red.');
-    const assert1 = (brain.execute('x'))[0].style.background === 'red'
+    const assert1 = brain.execute('x')[0].style.background === 'red'
     return assert1
 }
 
 function test6() {
     const brain = getBrain({ root: document.body })
     brain.execute('x is a button. the background of style of x is green.');
-    const assert1 = (brain.execute('x'))[0].style.background === 'green'
+    const assert1 = brain.execute('x')[0].style.background === 'green'
     return assert1
 }
 
@@ -79,24 +79,24 @@ function test6() {
 function test7() {
     const brain = getBrain({ root: document.body })
     brain.execute('x is a button. y is a button. z is a button. every button is red.')
-    const assert1 = (brain.execute('x'))[0].style.background === 'red'
-    const assert2 = (brain.execute('y'))[0].style.background === 'red'
-    const assert3 = (brain.execute('z'))[0].style.background === 'red'
+    const assert1 = brain.execute('x')[0].style.background === 'red'
+    const assert2 = brain.execute('y')[0].style.background === 'red'
+    const assert3 = brain.execute('z')[0].style.background === 'red'
     return assert1 && assert2 && assert3
 }
 
 function test8() {
     const brain = getBrain({ root: document.body })
     brain.execute('x is a button. text of x is capra.')
-    const assert1 = (brain.execute('button'))[0].textContent == 'capra'
+    const assert1 = brain.execute('button')[0].textContent == 'capra'
     return assert1
 }
 
 function test9() {
     const brain = getBrain({ root: document.body })
     brain.execute('x is a red button. x is green.')
-    const assert1 = (brain.execute('red')).length === 0
-    const assert2 = (brain.execute('green')).length === 1
+    const assert1 = brain.execute('red').length === 0
+    const assert2 = brain.execute('green').length === 1
     return assert1 && assert2
 }
 
