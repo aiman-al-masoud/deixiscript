61ddeb0c2c78342ee1b18b86ab96f3c2bc7f2f1d
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-02-25 19:34:37 +0100

    LexemeObject in own file

--- app/src/frontend/lexer/Lexeme.ts
+++ app/src/frontend/lexer/Lexeme.ts
@@ -1,29 +1,22 @@
 import { LexemeType } from "../../config/LexemeType"
-import Wrapper from "../../backend/wrapper/Wrapper"
-import { conjugate } from "./functions/conjugate"
-import { pluralize } from "./functions/pluralize"
-import { Cardinality, isRepeatable } from "../parser/interfaces/Cardinality"
+import { Cardinality } from "../parser/interfaces/Cardinality"
 import { Context } from "../../facade/context/Context"
+import LexemeObject from "./LexemeObject"
 
 
 export interface Lexeme {
     /**canonical form*/ readonly root: string
     /**token type*/ readonly type: LexemeType
-    /**made up of more lexemes*/ readonly contractionFor?: string[]
     /**form of this instance*/readonly token?: string
+    /**made up of more lexemes*/ readonly contractionFor?: string[]
     /**for quantadj */ readonly cardinality?: Cardinality
     readonly proto?: string
     readonly concepts?: string[]
     readonly _root?: Partial<Lexeme>
     readonly isPlural: boolean
     readonly isConcept: boolean
-
     extrapolate(context: Context): Lexeme[]
 }
-
-const thisIsRidiculous: (keyof Lexeme)[] = ['contractionFor', 'token', 'cardinality', 'proto', 'concepts']
-
-
 
 export function makeLexeme(data: Partial<Lexeme>): Lexeme {
 
@@ -32,60 +25,5 @@ export function makeLexeme(data: Partial<Lexeme>): Lexeme {
     }
 
     return new LexemeObject(data)
-
-}
-
-
-class LexemeObject implements Lexeme /*, Wrapper */ {
-
-    readonly _root = this.newData?._root
-
-    constructor(
-        readonly newData?: Partial<Lexeme>
-    ) {
-        thisIsRidiculous.forEach(k => {
-            (this as any)[k] = (newData as any)[k] ?? (this._root ?? {})[k]
-        })
-
-    }
-
-    get root() {
-
-        if (this._root) {
-            return this._root.root
-        }
-
-        return this.newData?.root as any
-    }
-
-    get type(): LexemeType {
-
-        if (this._root) {
-            return (this._root as Lexeme).type
-        }
-
-        return this.newData?.type as any
-    }
-
-    get isPlural() {
-        return isRepeatable(this.newData?.cardinality)
-    }
-
-    get isConcept() {
-        return this?.type === 'noun' && (this as any).concepts && !(this as any).proto
-    }
-
-    extrapolate(context: Context): Lexeme[] {
-
-        if ((this.type === 'noun' || this.type === 'grammar') && !this.isPlural) {
-            return [makeLexeme({ _root: this, token: pluralize(this.root), cardinality: '*' })]
-        }
-
-        if (['iverb', 'mverb'].includes(this.type)) {
-            return conjugate(this.root).map(x => makeLexeme({ _root: this, token: x }))
-        }
-
-        return []
-    }
 
 }
---
+++ app/src/frontend/lexer/LexemeObject.ts
@@ -0,0 +1,61 @@
+import { LexemeType } from "../../config/LexemeType"
+import { Context } from "../../facade/context/Context"
+import { isRepeatable } from "../parser/interfaces/Cardinality"
+import { conjugate } from "./functions/conjugate"
+import { pluralize } from "./functions/pluralize"
+import { Lexeme, makeLexeme } from "./Lexeme"
+
+export default class LexemeObject implements Lexeme /*, Wrapper */ {
+
+    readonly _root = this.newData?._root
+    readonly contractionFor = this.newData?.contractionFor ?? this._root?.contractionFor
+    readonly token = this.newData?.token ?? this._root?.token
+    readonly cardinality = this.newData?.cardinality ?? this._root?.cardinality
+    readonly proto = this.newData?.proto ?? this._root?.proto
+    readonly concepts = this.newData?.concepts ?? this._root?.concepts
+
+    constructor(
+        readonly newData?: Partial<Lexeme>
+    ) {
+    }
+
+    get root() {
+
+        if (this._root) {
+            return this._root.root
+        }
+
+        return this.newData?.root as any
+    }
+
+    get type(): LexemeType {
+
+        if (this._root) {
+            return (this._root as Lexeme).type
+        }
+
+        return this.newData?.type as any
+    }
+
+    get isPlural() {
+        return isRepeatable(this.newData?.cardinality)
+    }
+
+    get isConcept() {
+        return this?.type === 'noun' && (this as any).concepts && !(this as any).proto
+    }
+
+    extrapolate(context: Context): Lexeme[] {
+
+        if ((this.type === 'noun' || this.type === 'grammar') && !this.isPlural) {
+            return [makeLexeme({ _root: this, token: pluralize(this.root), cardinality: '*' })]
+        }
+
+        if (['iverb', 'mverb'].includes(this.type)) {
+            return conjugate(this.root).map(x => makeLexeme({ _root: this, token: x }))
+        }
+
+        return []
+    }
+
+}
