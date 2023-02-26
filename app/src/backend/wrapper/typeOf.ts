71e88cae82db1a2295e94c4bbd024cfaa2a3dbcc
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-02-27 00:30:11 +0100

    ...

--- app/src/backend/wrapper/typeOf.ts
+++ app/src/backend/wrapper/typeOf.ts
@@ -3,16 +3,11 @@ import { LexemeType } from "../../config/LexemeType"
 export function typeOf(o: object): LexemeType | undefined {
 
     if (typeof o === 'function') {
-        return (o.length ?? 0) > 0 ? 'mverb' : 'iverb'
-    }
-
-    if (typeof o === 'boolean') {
+        return o.length > 0 ? 'mverb' : 'iverb'
+    } else if (typeof o === 'boolean') {
         return 'adjective'
+    } else if (o) {
+        return 'noun'
     }
 
-    if (o === undefined) {
-        return undefined
-    }
-
-    return 'noun'
 }
