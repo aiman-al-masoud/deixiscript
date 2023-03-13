7ceb335d6397cee9b4777e7958a318bb37c15ab1
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-03-13 13:47:08 +0100

    ref...

--- app/src/middle/clauses/Imply.ts
+++ app/src/middle/clauses/Imply.ts
@@ -36,41 +36,22 @@ export default class Imply implements Clause {
         opts?.exactIds ?? this.exactIds
     )
 
-    flatList(): Clause[] {
-        return [this]
-    }
-
-    get entities(): Id[] {
-        return uniq(this.condition.entities.concat(this.consequence.entities))
-    }
-
-    implies(conclusion: Clause): Clause {
-        throw new Error('not implemented!')
-    }
-
-    about(id: Id): Clause {
-        // return emptyClause ///TODO!!!!!!!!
-        return this.condition.about(id).and(this.consequence.about(id))
-    }
-
     toString() {
         const yes = `${this.subjconj?.root ?? ''} ${this.condition.toString()} ---> ${this.consequence.toString()}`
         return this.negated ? `not(${yes})` : yes
     }
 
-    ownedBy(id: Id): Id[] {
-        return this.condition.ownedBy(id).concat(this.consequence.ownedBy(id))
-    }
-
-    ownersOf(id: Id): Id[] {
-        return this.condition.ownersOf(id).concat(this.consequence.ownersOf(id))
-    }
-
-    describe(id: Id): Lexeme[] {
-        return this.consequence.describe(id).concat(this.condition.describe(id))
-    }
+    flatList = () => [this]
+    ownedBy = (id: Id) => this.condition.ownedBy(id).concat(this.consequence.ownedBy(id))
+    ownersOf = (id: Id) => this.condition.ownersOf(id).concat(this.consequence.ownersOf(id))
+    describe = (id: Id) => this.consequence.describe(id).concat(this.condition.describe(id))
+    about = (id: Id) => this.condition.about(id).and(this.consequence.about(id))
 
     query(clause: Clause): Map[] {// TODO
+        throw new Error('not implemented!')
+    }
+
+    implies(conclusion: Clause): Clause {
         throw new Error('not implemented!')
     }
 
@@ -79,5 +60,9 @@ export default class Imply implements Clause {
             clause1: this.condition.simple,
             clause2: this.consequence.simple
         })
+    }
+
+    get entities(): Id[] {
+        return uniq(this.condition.entities.concat(this.consequence.entities))
     }
 }
