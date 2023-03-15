36a878892b5c624a8a03cc65479015862ac5d958
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-03-15 16:07:57 +0100

    just cleaning up

--- app/src/frontend/lexer/LexemeObject.ts
+++ app/src/frontend/lexer/LexemeObject.ts
@@ -1,4 +1,3 @@
-import { LexemeType } from "../../config/LexemeType"
 import { Context } from "../../facade/context/Context"
 import { isRepeatable } from "../parser/interfaces/Cardinality"
 import { conjugate } from "./functions/conjugate"
@@ -10,44 +9,29 @@ import { makeSetter } from "./makeSetter"
 export default class LexemeObject implements Lexeme {
 
     _root = this.newData?._root
+    readonly root = this.newData?.root ?? this._root?.root!
+    readonly type = this.newData?.type ?? this._root?.type!
     contractionFor = this.newData?.contractionFor ?? this._root?.contractionFor
     token = this.newData?.token ?? this._root?.token
     cardinality = this.newData?.cardinality ?? this._root?.cardinality
+    readonly isVerb = this.type === 'mverb' || this.type === 'iverb'
+    readonly isPlural = isRepeatable(this.newData?.cardinality)
+    readonly isMultiWord = this.root.includes(' ')
+
     proto = this.newData?.proto ?? this._root?.proto
     concepts = this.newData?.concepts ?? this._root?.concepts
     heirlooms = this?.newData?.heirlooms ?? this._root?.heirlooms ?? []
-    readonly isVerb = this.type === 'mverb' || this.type === 'iverb'
 
     constructor(
         readonly newData?: Partial<Lexeme>
-    ) {
-
-    }
-
-    get root() {
-
-        if (this._root) {
-            return this._root.root
-        }
-
-        return this.newData?.root as any
-    }
-
-    get type(): LexemeType {
-
-        if (this._root) {
-            return (this._root as Lexeme).type
-        }
-
-        return this.newData?.type as any
-    }
-
-    get isPlural() {
-        return isRepeatable(this.newData?.cardinality)
-    }
+    ) { }
 
     get isConcept() {
         return this?.type === 'noun' && (this as any).concepts && !(this as any).proto
+    }
+
+    getProto(): object | undefined {//TODO: maybe return Object.prototype by default
+        return (window as any)?.[this.proto as any]?.prototype;
     }
 
     extrapolate(context: Context): Lexeme[] {
@@ -56,19 +40,11 @@ export default class LexemeObject implements Lexeme {
             return [makeLexeme({ _root: this, token: pluralize(this.root), cardinality: '*' })]
         }
 
-        if (['iverb', 'mverb'].includes(this.type)) {
+        if (this.isVerb) {
             return conjugate(this.root).map(x => makeLexeme({ _root: this, token: x }))
         }
 
         return []
-    }
-
-    get isMultiWord() {
-        return this.root.includes(' ');
-    }
-
-    getProto(): object | undefined {//TODO: maybe return Object.prototype by default
-        return (window as any)?.[this.proto as any]?.prototype;
     }
 
     setAlias = (name: string, path: string[]) => {
