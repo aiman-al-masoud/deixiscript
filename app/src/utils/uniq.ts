bc20208ddfcf39cfbb8639d18d9319f98ca65b60
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-03-16 19:34:18 +0100

    better uniqe

--- app/src/utils/uniq.ts
+++ app/src/utils/uniq.ts
@@ -3,4 +3,10 @@
  * Remove duplicates from a list of primitives (numbers, bools, strings).
  * Careful using this with objects.
  */
-export const uniq = (x: any[]) => Array.from(new Set(x))
+// export const uniq = (x: any[]) => Array.from(new Set(x))
+
+
+export function uniq(a: any[]) {
+    var seen = {} as any
+    return a.filter(item => seen.hasOwnProperty(item) ? false : (seen[item] = true))
+}
---
+++ app/tests/newUnification.ts
@@ -0,0 +1,31 @@
+// import { Map } from "../src/middle/id/Map";
+// import { hashString } from "../src/utils/hashString";
+// import { uniq } from "../src/utils/uniq";
+
+// const testData: Map[] = [
+//     { x: 1 },
+//     { x: 10 },
+//     { y: 2 },
+//     { y: 11 },
+//     { z: 3 },
+//     { z: 12 },
+//     { x: 1, y: 2 },
+//     { x: 2, y: 3 },
+//     { x: 10, y: 11 },
+//     { x: 11, y: 12 },
+//     { y: 1, z: 2 },
+//     { y: 2, z: 3 },
+//     { y: 10, z: 11 },
+//     { y: 11, z: 12 },
+//     { x: 1 },
+//     { x: 10 },
+// ]
+
+// function solveMaps(maps: Map[]): Map[] {
+
+//     // hashString()
+//     // maps.filter(x=>maps.filter(x=>))
+//     // uniq
+    
+
+// }
