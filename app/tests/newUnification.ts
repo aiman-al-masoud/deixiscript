7728d4d4cf9d3352e8a6a3e0935e6a049ca5aaf0
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-03-16 21:16:22 +0100

    unif writing & testing...

--- app/tests/newUnification.ts
+++ app/tests/newUnification.ts
@@ -3,39 +3,20 @@ import { Map } from "../src/middle/id/Map";
 import { uniq } from "../src/utils/uniq";
 
 const testData: Map[] = [
-    { x: 1 },
-    { x: 10 },
-    { y: 2 },
-    { y: 11 },
-    { z: 3 },
-    { z: 12 },
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 10, y: 11 },
-    { x: 11, y: 12 },
-    { y: 1, z: 2 },
-    { y: 2, z: 3 },
-    { y: 10, z: 11 },
-    { y: 11, z: 12 },
-    { x: 1 },
-    { x: 10 },
+    { x: 1 }, { x: 10 },
+    { x: 1, y: 2 }, { x: 2, y: 3 }, { x: 10, y: 11 }, { x: 11, y: 12 },
+    { y: 2 }, { y: 11 },
+    { y: 1, z: 2 }, { y: 2, z: 3 }, { y: 10, z: 11 }, { y: 11, z: 12 },
+    { z: 3 }, { z: 12 },
+    { x: 1 }, { x: 10 },
 ]
 
-const testData2 :Map[] = [// WRONG RESULT FOR THIS TEST!
-    { x: 1 },
-    { x: 10 },
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 10, y: 11 },
-    { x: 11, y: 12 },
-    { y: 2 },
-    { y: 11 },
-    { y: 1, z: 2 },
-    { y: 2, z: 3 },
-    { y: 10, z: 11 },
-    { y: 11, z: 12 },
-    { z: 3 },
-    { z: 12 },
+const testData2: Map[] = [// WRONG RESULT FOR THIS TEST!
+    { x: 1 }, { x: 10 },
+    { x: 1, y: 2 }, { x: 2, y: 3 }, { x: 10, y: 11 }, { x: 11, y: 12 },
+    { y: 2 }, { y: 11 },
+    { y: 1, z: 2 }, { y: 2, z: 3 }, { y: 10, z: 11 }, { y: 11, z: 12 },
+    { z: 3 }, { z: 12 },
     { x: 1 }
 ]
 
@@ -56,13 +37,12 @@ function solveMaps(maps: Map[]): Map[] {
 
     const oneEntryMaps = maps.filter(m => Object.values(m).length <= 1)
     const allValsOfMem = allVars(oneEntryMaps).map(x => ({ [x]: allValsOf(oneEntryMaps, x) })).reduce((a, b) => ({ ...a, ...b }))
-    const dedup = uniq(maps)
-    const valid = dedup.filter(m => !isInvalid(m, allValsOfMem))
+    const valid = maps.filter(m => !isInvalid(m, allValsOfMem))
 
     valid.forEach((m1, i) => {
         valid.forEach((m2, j) => {
 
-            if (i !== j && Object.values(m1).length && Object.values(m2).length && Object.entries(m1).some(e => m2[e[0]] === e[1])) {
+            if (i !== j && Object.entries(m1).some(e => m2[e[0]] === e[1])) {
                 valid[j] = { ...m2, ...m1 }
                 valid[i] = {}
             }
