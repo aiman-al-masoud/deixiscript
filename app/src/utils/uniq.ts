5a568e82e636f34c53b44e17181b9ec38c1591b1
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-03-16 19:35:47 +0100

    ...

--- app/src/utils/uniq.ts
+++ app/src/utils/uniq.ts
@@ -1,12 +1,8 @@
-
 /**
  * Remove duplicates from a list of primitives (numbers, bools, strings).
  * Careful using this with objects.
  */
-// export const uniq = (x: any[]) => Array.from(new Set(x))
-
-
 export function uniq(a: any[]) {
-    var seen = {} as any
+    const seen = {} as any
     return a.filter(item => seen.hasOwnProperty(item) ? false : (seen[item] = true))
 }
