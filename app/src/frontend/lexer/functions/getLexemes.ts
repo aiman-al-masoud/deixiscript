955ccae7f2fe3d5ab5f80c4bef280a965994acf8
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-03-29 23:19:59 +0200

    rm numberLexeme()

--- app/src/frontend/lexer/functions/getLexemes.ts
+++ app/src/frontend/lexer/functions/getLexemes.ts
@@ -1,13 +1,11 @@
 import { Context } from "../../../facade/context/Context"
 import { Lexeme } from "../Lexeme"
 import { dynamicLexeme } from "./dynamicLexeme"
-import { numberLexeme } from "./numberLexeme"
 
 
 export function getLexemes(word: string, context: Context, words: string[]): Lexeme[] {
 
     const lex = context.getLexeme(word) ??
-        numberLexeme(word) ??
         dynamicLexeme(word, context, words)
 
     return lex.contractionFor ?
