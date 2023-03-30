6c32ac24101fc70f59a071f3413c9eae2986def1
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-03-30 13:22:37 +0200

    deepCopy() cloneNode(true)

--- app/src/utils/deepCopy.ts
+++ app/src/utils/deepCopy.ts
@@ -1,7 +1,7 @@
 export function deepCopy(object: object) {
 
     if (object instanceof HTMLElement) {
-        const wrapped = object.cloneNode() as HTMLElement
+        const wrapped = object.cloneNode(true) as HTMLElement
         wrapped.innerHTML = object.innerHTML
         return wrapped
     } else {
