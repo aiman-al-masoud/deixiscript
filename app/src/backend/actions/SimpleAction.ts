32d700cdbcdeb1193fb65f5e7bb6f5d88eba471d
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-04-04 00:08:04 +0200

    refactoring SimpleAction

--- app/src/backend/actions/SimpleAction.ts
+++ app/src/backend/actions/SimpleAction.ts
@@ -4,7 +4,6 @@ import { Context } from "../../facade/context/Context";
 import { wrap } from "../wrapper/Thing";
 import { getIncrementalId } from "../../middle/id/functions/getIncrementalId";
 import CreateLexemeAction from "./CreateLexemeAction";
-import { Map } from "../../middle/id/Map";
 import Imply from "../../middle/clauses/Imply";
 
 export default class SimpleAction implements Action {
@@ -16,15 +15,13 @@ export default class SimpleAction implements Action {
     run(context: Context) {
 
         const maps = context.query(this.topLevel.theme)
-        const maps2 = !maps.length ? this.getDefaultMap(this.clause, context) : maps
+        const maps2 = !maps.length ? [{}] : maps
         const maps3 = this.clause instanceof Imply ? maps2 : maps2.slice(0, 1)
-
 
         maps3.forEach(m => {
 
             const argz = this.clause.args! ?? this.clause.entities
             const predicate = this.clause.predicate! ?? this.clause.rheme.predicate
-
 
             if (this.topLevel.flatList().some(x => x.predicate?.type === 'grammar')) {
                 return new CreateLexemeAction(this.clause, this.topLevel).run(context)
@@ -33,10 +30,9 @@ export default class SimpleAction implements Action {
             const args = argz
                 .map(id => m[id] ? context.get(m[id])! : context.set(wrap({ id: getIncrementalId() })))
 
-
             const subject = args[0]
 
-            /* const res =  */subject?.set(predicate, {
+            subject?.set(predicate, {
                 args: args.slice(1),
                 context,
                 negated: this.clause.negated
@@ -47,24 +43,7 @@ export default class SimpleAction implements Action {
                 context.setLexeme(predicate)
             }
 
-            // return res//TODO save to context
-
         })
-
-    }
-
-    protected getDefaultMap(clause: Clause, context: Context): Map[] {
-
-        if (clause instanceof Imply) {
-            return []
-        }
-        if (this.topLevel.flatList().some(x => x.predicate?.type === 'grammar')) {
-            return [{}]
-        }
-
-        return [(clause.args ?? [])
-            .map(id => ({ [id]: context.set(wrap({ id: getIncrementalId() })).id }))
-            .reduce((a, b) => ({ ...a, ...b }))]
 
     }
 
