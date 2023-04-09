c0007728b1814126e3ceb07745162ed00a704a36
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-04-09 22:44:38 +0200

    rm old

--- app/src/middle/clauses/functions/getOwnershipChain.ts
+++ app/src/middle/clauses/functions/getOwnershipChain.ts
@@ -1,12 +1,12 @@
-import { Clause } from "../Clause"
-import { Id } from "../../id/Id"
+// import { Clause } from "../Clause"
+// import { Id } from "../../id/Id"
 
-export function getOwnershipChain(clause: Clause, entity: Id): Id[] {
+// export function getOwnershipChain(clause: Clause, entity: Id): Id[] {
 
-    const ownedEntities = clause.ownedBy(entity)
+//     const ownedEntities = clause.ownedBy(entity)
 
-    return ownedEntities.length === 0 ?
-        [entity] :
-        [entity].concat(getOwnershipChain(clause, ownedEntities[0]))
+//     return ownedEntities.length === 0 ?
+//         [entity] :
+//         [entity].concat(getOwnershipChain(clause, ownedEntities[0]))
 
-}
+// }
--- app/src/middle/clauses/functions/topLevel.ts
+++ app/src/middle/clauses/functions/topLevel.ts
@@ -1,9 +1,9 @@
-import { Clause } from "../Clause";
+// import { Clause } from "../Clause";
 
-export function getTopLevel(clause: Clause) {
-    return clause
-        .entities
-        .map(x => ({ x, owners: clause.ownersOf(x) }))
-        .filter(x => x.owners.length === 0)
-        .map(x => x.x)
-}
+// export function getTopLevel(clause: Clause) {
+//     return clause
+//         .entities
+//         .map(x => ({ x, owners: clause.ownersOf(x) }))
+//         .filter(x => x.owners.length === 0)
+//         .map(x => x.x)
+// }
