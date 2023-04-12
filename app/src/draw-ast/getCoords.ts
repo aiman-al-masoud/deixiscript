d6fb91d480f7d184e1a641083e7c875bf201e40c
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-04-12 14:04:07 +0200

    refactoring getCoords()

--- app/src/draw-ast/getCoords.ts
+++ app/src/draw-ast/getCoords.ts
@@ -2,30 +2,27 @@
 export function getCoords(
     initialPos: Coordinate,
     data: EdgeList,
-    dict: { [x: string]: Coordinate } = {}
+    oldCoords: { [x: string]: Coordinate } = {}
 ): { [x: string]: Coordinate } {
 
     const root = getRoot(data) // node w/out parents
 
     if (!root) {
-        return dict
+        return oldCoords
     }
 
     const children = getChildrenOf(root, data)
-    const rootPos = dict[root] ?? initialPos
+    const rootPos = oldCoords[root] ?? initialPos
 
     const yOffset = 30
     const xOffset = 30
 
-    const xDisplacements = children
-        .map((_, i) => i * xOffset * (i < children.length / 2 ? -1 : 1)) //+ 30*Math.random()
-
-    const childMap = children
-        .map((c, i) => ({ [c]: { x: rootPos.x + xDisplacements[i], y: rootPos.y + yOffset } }))
+    const childCoords = children
+        .map((c, i) => ({ [c]: { x: rootPos.x + i * xOffset * (i < children.length / 2 ? -1 : 1), y: rootPos.y + yOffset } }))
         .reduce((a, b) => ({ ...a, ...b }), {})
 
     const remainingData = data.filter(x => !x.includes(root))
-    const partialResult = { ...dict, ...childMap, ...{ [root]: rootPos } }
+    const partialResult = { ...oldCoords, ...childCoords, ...{ [root]: rootPos } }
 
     return getCoords(initialPos, remainingData, partialResult)
 }
