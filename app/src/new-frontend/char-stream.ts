02c15dc78a7daf78f5d2f604e78c67c52ccf7a57
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-01 12:43:25 +0200

    acc

--- app/src/new-frontend/char-stream.ts
+++ app/src/new-frontend/char-stream.ts
@@ -1,8 +1,25 @@
 export interface CharStream {
+
+    /**
+     * Add the next char to the accumulator
+     */
     next(): void
+    /**
+     * Read the accumulated value
+     */
     peekAcc(): string
+    /**
+     * Clear the accumulated value
+     */
     clearAcc(): void
+    /**
+     * Go back AND clear the accumulated value.
+     */
     backTo(pos: number): void
+    /**
+     * Get the current position
+     */
+    getPost(): number
 }
 
 
