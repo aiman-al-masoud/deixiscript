ebb49c9693116b98a7a6aecd7422d881fff62f6b
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-02 15:57:44 +0200

    ...

--- app/index.ts
+++ app/index.ts
@@ -14,18 +14,18 @@ import { parseSyntax, parseTry } from "./src/new-frontend/parser";
 // EXAMPLE 1
 const cs = getCharStream('12    mondo ')
 const x = parseTry(['number-literal'], cs)
-console.log(x)
+console.log(1, x)
 // EXAMPLE 2
 const cs2 = getCharStream('do not make ')// also try without not
 const x2 = parseTry(['do-verb'], cs2)
-console.log(x2)
+console.log(2, x2)
 // EXAMPLE 3
 const cs3 = getCharStream('" ciao "xxx')
 const x3 = parseTry(['string-literal'], cs3)
-console.log(x3)
+console.log(3, x3)
 // EXAMPLE 4
 const cs4 = getCharStream('ciao mondo buruf')
 const x4 = parseSyntax([{ types: ['identifier'], sep: 'space', number: 'all-but-last', role: 'anything' as any }], cs4)
 const x40 = parseTry(['identifier'], cs4)
-console.log(x4, x40)
+console.log(4, x4, x40)
 
