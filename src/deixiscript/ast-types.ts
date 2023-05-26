6da0553d66f927e78ae01342141235a42114a32f
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-26 18:33:33 +0200

    types

--- src/deixiscript/ast-types.ts
+++ src/deixiscript/ast-types.ts
@@ -1,36 +1,74 @@
 
-    export type copula_sentence = {
-        type : 'copula-sentence'
-        subject: noun_phrase
-	negation?: string
-	object: noun_phrase
-    }
-    
+type copula_sentence = {
+    type: 'copula-sentence'
+    subject: noun_phrase
+    negation?: string
+    object: noun_phrase
+}
 
 
-    export type noun_phrase = {
-        type : 'noun-phrase'
-        modifiers?: string[]
-	owner?: noun_phrase | string
-	head: string
-    }
-    
+
+type noun_phrase = {
+    type: 'noun-phrase'
+    modifiers?: string[]
+    owner?: noun_phrase | string
+    head: string
+    pluralizer?: string
+    suchThat?: sentence
+}
 
 
-    type number_literal = {
-        type : 'number-literal'
-        digits: string[]
-    }
-    
+
+type number_literal = {
+    type: 'number-literal'
+    digits: string[]
+}
 
 
-    type if_sentence = {
-        type : 'if-sentence'
-        condition: sentence
-	consequence: sentence
-    }
-    
 
-type sentence = copula_sentence|if_sentence
+type verb_sentence = {
+    type: 'verb-sentence'
+    subject: noun_phrase
+    negation?: string
+    verb: string
+    object?: noun_phrase
+}
 
-export type ast_node = copula_sentence|noun_phrase|number_literal|if_sentence|sentence
+
+
+type if_sentence = {
+    type: 'if-sentence'
+    condition: sentence
+    consequence: sentence
+}
+
+
+
+type comparative_sentence = {
+    type: 'comparative-sentence'
+    subject: noun_phrase
+    comparison: string
+    object: noun_phrase
+}
+
+
+
+type has_sentence = {
+    type: 'has-sentence'
+    subject: noun_phrase
+    object: noun_phrase
+    role: noun_phrase
+}
+
+
+
+type and_sentence = {
+    type: 'and-sentence'
+    first: sentence
+    second: sentence | and_sentence
+}
+
+
+type sentence = copula_sentence | if_sentence | verb_sentence | comparative_sentence | has_sentence
+
+export type ast_node = copula_sentence | noun_phrase | number_literal | verb_sentence | if_sentence | comparative_sentence | has_sentence | and_sentence | sentence
--- src/deixiscript/deixis.ts
+++ src/deixiscript/deixis.ts
@@ -1,94 +1,94 @@
-import { getParser } from '../parser/parser.ts'
-import { AstNode } from '../parser/types.ts'
-import { ast_node, noun_phrase } from './ast-types.ts'
-import { syntaxes } from './grammar.ts'
+// import { getParser } from '../parser/parser.ts'
+// import { AstNode } from '../parser/types.ts'
+// import { ast_node, noun_phrase } from './ast-types.ts'
+// import { syntaxes } from './grammar.ts'
 
-import { $, ExpBuilder } from '../machines-like-us/exp-builder.ts'
-import { Ast, WorldModel } from '../machines-like-us/types.ts'
-// import { getParts } from '../machines-like-us/wm-funcs.ts'
-import { findAll } from '../machines-like-us/findAll.ts'
-
-
-export function parse(sourceCode: string): ast_node[] {
-
-    const results: AstNode[] = []
-
-    sourceCode.split('.').map(x => x.trim()).forEach(s => {
-        const ast = getParser(s, syntaxes).parse()
-        if (ast) {
-            results.push(ast)
-        }
-    })
-
-    return results as any
-}
+// import { $, ExpBuilder } from '../machines-like-us/exp-builder.ts'
+// import { Ast, WorldModel } from '../machines-like-us/types.ts'
+// // import { getParts } from '../machines-like-us/wm-funcs.ts'
+// import { findAll } from '../machines-like-us/findAll.ts'
 
 
-const res = parse(`
-    the cat's fur is green. 
-    the cat is big.
-    the cat is a smart cat.
-    if the cat is black then the dog is stupid.
-    `)
+// export function parse(sourceCode: string): ast_node[] {
 
-const wm: WorldModel = [
-    ['cat#33', 'cat'],
-    ['cat#44', 'cat'],
-    ['cat#44', 'red'],
-    ['dog#22', 'dog'],
-]
+//     const results: AstNode[] = []
 
+//     sourceCode.split('.').map(x => x.trim()).forEach(s => {
+//         const ast = getParser(s, syntaxes).parse()
+//         if (ast) {
+//             results.push(ast)
+//         }
+//     })
 
-function findEntity(np: noun_phrase, wm: WorldModel) {
-
-    const v = $(`x:${np.head}`)
-    let query: ExpBuilder<Ast> = v.isa(np.head)
-
-    np.modifiers?.forEach(m => {
-        query = query.and(v.isa(m))
-    })
-
-    const results = findAll(query.$, [v.$], { wm, derivClauses: [] })
-    return results
-
-}
-
-console.log(findEntity(parse('the red cat')[0] as noun_phrase, wm))
+//     return results as any
+// }
 
 
-// function updateEntities(ast: ast_node, wm: WorldModel): [WorldModel, ast_node] {
+// const res = parse(`
+//     the cat's fur is green. 
+//     the cat is big.
+//     the cat is a smart cat.
+//     if the cat is black then the dog is stupid.
+//     `)
 
-//     switch (ast.type) {
-//         case 'noun-phrase':
-
-//             // search for it in wm
-//             {
-//                 const v = $(`x:${ast.head}`)
-//                 let query: ExpBuilder<Ast> = v.isa(ast.head)
-
-//                 ast.modifiers?.forEach(m => {
-//                     query = query.and(v.isa(m))
-//                 })
-
-//                 const results = findAll(query.$, [v.$], { wm, derivClauses: [] })
+// const wm: WorldModel = [
+//     ['cat#33', 'cat'],
+//     ['cat#44', 'cat'],
+//     ['cat#44', 'red'],
+//     ['dog#22', 'dog'],
+// ]
 
 
-//             }
+// function findEntity(np: noun_phrase, wm: WorldModel) {
 
-//             // if single result present, return old wm with updated ast
+//     const v = $(`x:${np.head}`)
+//     let query: ExpBuilder<Ast> = v.isa(np.head)
 
-//             // if multiple results, raise error
+//     np.modifiers?.forEach(m => {
+//         query = query.and(v.isa(m))
+//     })
 
-//             // if not present, create new entity
-
-//             return
-//         case 'if-sentence':
-//         // return updateEntities(ast.condition, wm).concat(updateEntities(ast.consequence, wm))
-//         case 'copula-sentence':
-//         // return updateEntities(ast.subject, wm).concat(updateEntities(ast.object, wm))
-//         case 'number-literal':
-//         // return wm
-//     }
+//     const results = findAll(query.$, [v.$], { wm, derivClauses: [] })
+//     return results
 
 // }
+
+// console.log(findEntity(parse('the red cat')[0] as noun_phrase, wm))
+
+
+// // function updateEntities(ast: ast_node, wm: WorldModel): [WorldModel, ast_node] {
+
+// //     switch (ast.type) {
+// //         case 'noun-phrase':
+
+// //             // search for it in wm
+// //             {
+// //                 const v = $(`x:${ast.head}`)
+// //                 let query: ExpBuilder<Ast> = v.isa(ast.head)
+
+// //                 ast.modifiers?.forEach(m => {
+// //                     query = query.and(v.isa(m))
+// //                 })
+
+// //                 const results = findAll(query.$, [v.$], { wm, derivClauses: [] })
+
+
+// //             }
+
+// //             // if single result present, return old wm with updated ast
+
+// //             // if multiple results, raise error
+
+// //             // if not present, create new entity
+
+// //             return
+// //         case 'if-sentence':
+// //         // return updateEntities(ast.condition, wm).concat(updateEntities(ast.consequence, wm))
+// //         case 'copula-sentence':
+// //         // return updateEntities(ast.subject, wm).concat(updateEntities(ast.object, wm))
+// //         case 'number-literal':
+// //         // return wm
+// //     }
+
+// // }
 
