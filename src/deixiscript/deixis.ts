2b28fca22bb02b44cb3a7a59ddd87d560d7421cb
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-27 17:31:42 +0200

    ...

--- src/deixiscript/deixis.ts
+++ src/deixiscript/deixis.ts
@@ -1,16 +1,38 @@
-import { LLangAst, WorldModel } from "../machines-like-us/types.ts";
-import { noun_phrase } from "./ast-types.ts";
+import { Conjunction, LLangAst } from "../machines-like-us/types.ts";
+import { ast_node } from "./ast-types.ts";
 import { copulaToHas } from "./copulaToHas.ts";
 import { expandModifiers } from "./expandModifiers.ts";
 import { parse } from "./parse.ts";
 import { wm } from './example-world-model.ts'
+import { $, ExpBuilder } from "../machines-like-us/exp-builder.ts";
 
-console.log(copulaToHas(expandModifiers(parse('the red button')), wm))
 
-// function convert(np: noun_phrase): LLangAst {
+function convert(ast: ast_node): ExpBuilder<LLangAst> {
 
-//     if (np.modifiers) {
-//         throw new Error('noun phrase has modifiers!')
-//     }
+    switch (ast.type) {
+        case 'noun-phrase':
 
-// }
+            if (ast.modifiers) throw new Error('noun phrase has modifiers!')
+            if (ast.owner !== undefined) throw new Error('noun phrase has owner!')
+
+            {
+                let query: ExpBuilder<LLangAst> = $('x:thing').isa(ast.head)
+
+                if (ast.suchThat) {
+                    query = query.and(convert(ast.suchThat) as ExpBuilder<Conjunction>)
+                }
+
+                return query
+            }
+
+        case 'has-sentence':
+            return $('x:thing').has(ast.object.head).as(ast.role.head)
+
+    }
+    
+    throw new Error('not implemented!')
+}
+
+
+const ast = copulaToHas(expandModifiers(parse('the red button')), wm)
+console.log(convert(ast))
