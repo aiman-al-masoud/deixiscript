018e41249ce293a9eac550291986653eb5d1c1c6
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-27 16:07:58 +0200

    ...

---
+++ src/deixiscript/verbalize.ts
@@ -0,0 +1,16 @@
+// import { SyntaxMap } from "../parser/types.ts";
+// import { ast_node } from "./ast-types.ts";
+// import { syntaxes } from "./grammar.ts";
+// import { parse } from "./parse.ts";
+
+// function verbalize(ast: ast_node, syntaxes: SyntaxMap): string {
+//     switch (ast.type) {
+
+//         case 'noun-phrase':
+//             console.log(syntaxes['noun-phrase'])
+
+//     }
+// }
+
+// verbalize(parse('the button'), syntaxes)
+
