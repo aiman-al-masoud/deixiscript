419951630bc002106228d4faaac22cddb8e99146
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-28 15:05:27 +0200

    ...

--- src/deixiscript/resImplRefs.ts
+++ src/deixiscript/resImplRefs.ts
@@ -7,56 +7,43 @@ import { wm } from './example-world-model.ts'
 import { $, ExpBuilder } from "../machines-like-us/exp-builder.ts";
 import { findAll } from "../machines-like-us/findAll.ts";
 
-export function resImplRefs(
-    asts: ast_node[],
-    wm: WorldModel,
-): [ast_node[], WorldModel] {
-
-    return [
-        asts.map(ast => oneStep(ast, wm)),
-        wm,
-    ]
-}
-
-function oneStep<T extends ast_node>(ast: ast_node, wm: WorldModel): T
-function oneStep(ast: ast_node, wm: WorldModel): ast_node {
+function resImpRefs<T extends ast_node>(ast: ast_node, wm: WorldModel): T
+function resImpRefs(ast: ast_node, wm: WorldModel): ast_node {
 
     switch (ast.type) {
         case 'noun-phrase':
-            {
-                const variable = `${ast.head}${generateRandom()}:${ast.head}` as const
-                const query = astToQuery(ast, variable)
-                const result = findAll(query.$, [$(variable).$], { wm, derivClauses: [] })
-                let id: string
+            const variable = `${ast.head}${generateRandom()}:${ast.head}` as const
+            const query = astToQuery(ast, variable)
+            const result = findAll(query.$, [$(variable).$], { wm, derivClauses: [] })
+            let id: string
 
-                if (result.length > 1 && !ast.pluralizer) {
-                    throw new Error('ambiguous: multiple anaphoric matches!')
-                } else if (result.length === 1) {
-                    const map = result[0]
-                    const match = map.get($(variable).$)
-                    id = match?.value!
-                } else if (!result.length) {
-                    id = ast.head + generateRandom()
-                    updateWorldModel(ast, wm, id)
-                }
+            if (!result.length) {
+                id = ast.head + generateRandom()
+                updateWorldModel(ast, wm, id)
+            } else if (result.length === 1) {
+                const map = result[0]
+                const match = map.get($(variable).$)
+                id = match?.value!
+            } else if (result.length > 1 && !ast.pluralizer) {
+                throw new Error('ambiguous: multiple anaphoric matches!')
+            }
 
-                return {
-                    type: 'noun-phrase',
-                    head: id!,
-                }
+            return {
+                type: 'noun-phrase',
+                head: id!,
             }
         case 'copula-sentence':
             return {
                 type: 'copula-sentence',
-                subject: oneStep(ast.subject, wm),
-                object: oneStep(ast.object, wm),
+                subject: resImpRefs(ast.subject, wm),
+                object: resImpRefs(ast.object, wm),
             }
         case 'has-sentence':
             return {
                 type: 'has-sentence',
-                subject: oneStep(ast.subject, wm),
-                object: oneStep(ast.object, wm),
-                role: oneStep(ast.role, wm),
+                subject: resImpRefs(ast.subject, wm),
+                object: resImpRefs(ast.object, wm),
+                role: resImpRefs(ast.role, wm),
             }
 
     }
@@ -81,16 +68,13 @@ function astToQuery(ast: ast_node, variable: string): ExpBuilder<LLangAst> {
             if (ast.modifiers) throw new Error('noun phrase has modifiers!')
             if (ast.owner !== undefined) throw new Error('noun phrase has owner!')
 
-            {
-                let query: ExpBuilder<LLangAst> = $(variable).isa(ast.head)
+            let query: ExpBuilder<LLangAst> = $(variable).isa(ast.head)
 
-                if (ast.suchThat) {
-                    query = query.and(astToQuery(ast.suchThat, variable) as ExpBuilder<Conjunction>)
-                }
-
-                return query
+            if (ast.suchThat) {
+                query = query.and(astToQuery(ast.suchThat, variable) as ExpBuilder<Conjunction>)
             }
 
+            return query
         case 'has-sentence':
             return $(variable).has(ast.object.head).as(ast.role.head)
 
@@ -106,6 +90,6 @@ function generateRandom() {
 const ast = copulaToHas(expandModifiers(parse('the red button')), wm)
 // const ast2 = parse('the button')
 // console.log(astToQuery(ast, 'button1:button'))
-console.log(oneStep(ast, wm))
+console.log(resImpRefs(ast, wm))
 console.log(wm)
-console.log(oneStep(ast, wm))
+console.log(resImpRefs(ast, wm))
