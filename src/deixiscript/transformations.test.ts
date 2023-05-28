bb04b3ca441f613e95a066b0b398432a6474d2f7
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-28 16:15:55 +0200

    ...

--- src/deixiscript/transformations.test.ts
+++ src/deixiscript/transformations.test.ts
@@ -1,9 +1,9 @@
 import { parse } from "./parse.ts";
-import { wm } from "./example-world-model.ts";
 import { expandModifiers } from "./expandModifiers.ts";
 import { copulaToHas } from "./copulaToHas.ts";
 import { resImpRefs } from "./resImplRefs.ts";
 import { useDef } from "./useDef.ts";
+import { WorldModel } from "../machines-like-us/types.ts";
 
 const sourceCode = `
 the player does jump to the enemy.
@@ -11,7 +11,10 @@ the player is a sprite.
 the player does die.
 `
 
-let wm1 = wm
+let wm1: WorldModel = [
+    ['sprite', 'thing'],
+]
+
 const sentences = sourceCode.split('.').map(x => x.trim()).filter(x => x)
 const asts = sentences.map(x => parse(x))
 const asts2 = asts.map(x => expandModifiers(x))
@@ -19,11 +22,5 @@ const asts3 = asts2.map(x => copulaToHas(x, wm1))
 const asts4 = asts3.map(x => resImpRefs(x, wm1))
 wm1 = useDef(asts4, wm1)
 console.log(wm1)
-
-
-
-
-
-
 
 
