010d265d5dd5ba014478db2b7cde8a53d0bb860f
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-30 14:11:52 +0200

    playing around with verbalize

--- src/deixiscript/verbalize.ts
+++ src/deixiscript/verbalize.ts
@@ -1,16 +1,31 @@
-// import { SyntaxMap } from "../parser/types.ts";
-// import { ast_node } from "./ast-types.ts";
-// import { syntaxes } from "./grammar.ts";
-// import { parse } from "./parse.ts";
+import { SyntaxMap } from "../parser/types.ts";
+import { ast_node } from "./deixi-ast.ts";
+import { syntaxes } from "./grammar.ts";
+import { parse } from "./parse.ts";
 
-// function verbalize(ast: ast_node, syntaxes: SyntaxMap): string {
-//     switch (ast.type) {
+function verbalize(ast: ast_node, /* syntaxes?: SyntaxMap=undefined */): string {
 
-//         case 'noun-phrase':
-//             console.log(syntaxes['noun-phrase'])
+    switch (ast.type) {
+        case 'noun-phrase':
+            if (ast.suchThat) return `( ${ast.head} such that ${verbalize(ast.suchThat!)} )`
+            // if (ast.modifiers) return 
+            return '(' + ast.head + ')'
+        case 'copula-sentence':
+            return `( ${verbalize(ast.subject)} is ${verbalize(ast.object)} )`
+        case 'there-is-sentence':
+            break
+        case 'has-sentence':
+            return `( ${verbalize(ast.subject)} has ${verbalize(ast.object)} as ${verbalize(ast.role)} )`
+        case 'and-sentence':
+            break
+        case 'comparative-sentence':
+            return `(${verbalize(ast.subject)} is more ${ast.comparison} than ${verbalize(ast.object)})`
+    }
 
-//     }
-// }
+    throw new Error('not implemented!')
+}
 
-// verbalize(parse('the button'), syntaxes)
+
+console.log(verbalize(parse('the button is more beautiful than the door')))
+
 
