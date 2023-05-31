c63f8025cb508f7a1973cb4ef960768b5a2bc692
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-06-01 00:59:25 +0200

    parseNumber((

---
+++ src/utils/parseNumber.ts
@@ -0,0 +1,20 @@
+/* 
+ * A non-lobotomized version
+ */
+
+/**
+ * Checks if string has some non-digit char (except for ".") before
+ * converting to number.
+ */
+export function parseNumber(string: string): number | undefined {
+
+    const nonDig = string.match(/\D/g)?.at(0)
+
+    if (nonDig && nonDig !== '.') {
+        return undefined
+    }
+
+    return parseFloat(string)
+
+}
+
