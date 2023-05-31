e0d8657975d81f04523c218d03669397acb8ed2d
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-05-31 22:31:42 +0200

    ...

--- src/machines-like-us/match.ts
+++ src/machines-like-us/match.ts
@@ -4,7 +4,7 @@ import { $ } from "./exp-builder.ts";
 import { formulasEqual } from "./formulasEqual.ts";
 import { getAtoms } from "./getAtoms.ts";
 import { substAll } from "./subst.ts";
-import { Atom, isVarish, LLangAst, Variable, VarMap } from "./types.ts";
+import { Atom, AtomicFormula, atomsEqual, isAtom, isAtomTruthy, isConst, isListLiteral, isVar, isVarish, LLangAst, Variable, VarMap } from "./types.ts";
 
 export function match(template: LLangAst, formula: LLangAst) {
 
@@ -55,3 +55,50 @@ export function match(template: LLangAst, formula: LLangAst) {
 // )
 
 // console.log(x)
+
+
+
+
+
+
+
+export function oldMatch(template: AtomicFormula, f: AtomicFormula): VarMap | undefined {
+
+    if (!isListLiteral(f.after)) {
+        return undefined
+    }
+
+    const fAfter = f.after
+    let afterMap: VarMap
+
+    if (isAtomTruthy(template.after) && !isAtomTruthy(f.after)) {
+        return undefined
+    }
+
+    if (isVar(template.after)) {
+        afterMap = deepMapOf([[template.after, { type: 'list-literal', list: fAfter.list.slice(0, -1) }]])
+    } else if (isConst(template.after) || template.after.type === 'boolean') {
+        afterMap = deepMapOf()
+    } else if (template.after.type === 'list-literal') {
+        afterMap = deepMapOf(template.after.list
+            .filter(isVar)
+            .map((x, i) => [x, fAfter.list[i]]))
+    } else if(isVar(template.after.seq)&&isVar(template.after.tail)){
+        afterMap = deepMapOf([[template.after.seq, { type: 'list-literal', list: fAfter.list.slice(0, -1) }], [template.after.tail, fAfter.list.at(-1)!]])
+    }else{
+        afterMap = deepMapOf()
+    }
+
+    const templateTerms = [template.t1, template.t2, ...(template.type === 'has-formula' ? [template.as] : [])]
+    const fTerms = [f.t1, f.t2, ...(f.type === 'has-formula' ? [f.as] : [])]
+
+    const zipped = templateTerms.map((x, i) => [x, fTerms[i]] as const)
+    const disagree = zipped.some(e => (!e[0] || !e[1]) || isConst(e[0]) && isConst(e[1]) && !atomsEqual(e[0], e[1]))
+    const reduced = zipped.filter(e => e[0] !== e[1]).filter(e => isVar(e[0])) as [Variable, Atom][]
+
+    if (!disagree) {
+        return deepMapOf([...afterMap, ...reduced])
+    }
+
+}
+
--- src/machines-like-us/test.ts
+++ src/machines-like-us/test.ts
@@ -3,7 +3,7 @@ import { findAll, } from "./findAll.ts";
 import { substAll } from "./subst.ts";
 import { getSupers } from "./wm-funcs.ts";
 import { deepMapOf } from "../utils/DeepMap.ts";
-import { match } from "./match.ts";
+import { match, oldMatch } from "./match.ts";
 
 export function test(formula: LLangAst, kb: KnowledgeBase): boolean {
 
@@ -75,44 +75,3 @@ export function test(formula: LLangAst, kb: KnowledgeBase): boolean {
     })
 
 }
-
-function oldMatch(template: AtomicFormula, f: AtomicFormula): VarMap | undefined {
-
-    if (!isListLiteral(f.after)) {
-        return undefined
-    }
-
-    const fAfter = f.after
-    let afterMap: VarMap
-
-    if (isAtomTruthy(template.after) && !isAtomTruthy(f.after)) {
-        return undefined
-    }
-
-    if (isVar(template.after)) {
-        afterMap = deepMapOf([[template.after, { type: 'list-literal', list: fAfter.list.slice(0, -1) }]])
-    } else if (isConst(template.after) || template.after.type === 'boolean') {
-        afterMap = deepMapOf()
-    } else if (template.after.type === 'list-literal') {
-        afterMap = deepMapOf(template.after.list
-            .filter(isVar)
-            .map((x, i) => [x, fAfter.list[i]]))
-    } else if(isVar(template.after.seq)&&isVar(template.after.tail)){
-        afterMap = deepMapOf([[template.after.seq, { type: 'list-literal', list: fAfter.list.slice(0, -1) }], [template.after.tail, fAfter.list.at(-1)!]])
-    }else{
-        afterMap = deepMapOf()
-    }
-
-    const templateTerms = [template.t1, template.t2, ...(template.type === 'has-formula' ? [template.as] : [])]
-    const fTerms = [f.t1, f.t2, ...(f.type === 'has-formula' ? [f.as] : [])]
-
-    const zipped = templateTerms.map((x, i) => [x, fTerms[i]] as const)
-    const disagree = zipped.some(e => (!e[0] || !e[1]) || isConst(e[0]) && isConst(e[1]) && !atomsEqual(e[0], e[1]))
-    const reduced = zipped.filter(e => e[0] !== e[1]).filter(e => isVar(e[0])) as [Variable, Atom][]
-
-    if (!disagree) {
-        return deepMapOf([...afterMap, ...reduced])
-    }
-
-}
-
