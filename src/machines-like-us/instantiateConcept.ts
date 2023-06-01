081f83c7f32a5e53edf0761ca198dbc5f44206a9
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-06-01 12:31:59 +0200

    ...

---
+++ src/machines-like-us/instantiateConcept.ts
@@ -0,0 +1,12 @@
+import { KnowledgeBase, WorldModel } from "./types.ts";
+
+
+/**
+ * Uses default values to create a new instance of a concept.
+ */
+export function instantiateConcept(
+    concept: string,
+    kb: KnowledgeBase,
+): WorldModel {
+    throw new Error('error!')
+}
