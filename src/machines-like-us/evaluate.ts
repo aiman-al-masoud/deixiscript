8767dab9e1685ef3e0fc9e499b6bfddced4c4185
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-06-24 13:04:11 +0200

    ...

--- src/machines-like-us/evaluate.ts
+++ src/machines-like-us/evaluate.ts
@@ -1,12 +1,12 @@
-import { $ } from "./exp-builder.ts";
-import { recomputeKb } from "./recomputeKb.ts";
-import { test } from "./test.ts";
-import { Atom, KnowledgeBase, LLangAst } from "./types.ts";
+// import { $ } from "./exp-builder.ts";
+// import { recomputeKb } from "./recomputeKb.ts";
+// import { test } from "./test.ts";
+// import { Atom, KnowledgeBase, LLangAst } from "./types.ts";
 
-export function evaluate(ast: LLangAst, isCommand: boolean, kb: KnowledgeBase): [KnowledgeBase, Atom] {
-    if (isCommand) {
-        return [recomputeKb(ast, kb), $(true).$]
-    } else {
-        return [kb, test(ast, kb) as Atom]
-    }
-}
+// export function evaluate(ast: LLangAst, isCommand: boolean, kb: KnowledgeBase): [KnowledgeBase, Atom] {
+//     if (isCommand) {
+//         return [recomputeKb(ast, kb), $(true).$]
+//     } else {
+//         return [kb, test(ast, kb) as Atom]
+//     }
+// }
