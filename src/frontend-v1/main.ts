a44cbb351ef7eca00399ceece7672c33a39716b9
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-06-27 10:57:07 +0200

    ...

--- src/frontend-v1/main.ts
+++ src/frontend-v1/main.ts
@@ -14,14 +14,9 @@ let kb = getStandardKb()
 while (true) {
     const x = prompt('> ')!
     const ast0 = parser.parse(x) as any
-    // console.log(ast0)
-    // const ast1 = 'f1' in ast0 ? ast0['f1'] : ast0
-    const ast = cleanUpAst(ast0)
-    console.log(ast)
-    //TODO: command or question
-
-    // console.log(ast)
+    const ast1 = ast0.type === 'command' || ast0.type === 'question' ? ast0['f1'] : ast0
     const isCommand = ast0.type === 'command'
+    const ast = cleanUpAst(ast1)
     const result = evaluate({ ast, isCommand, kb })
     console.log('===>', result.result)
     kb = result.kb
