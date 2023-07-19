74c1f5ed6de312bfc005d58d07307bfff6e29c7b
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-07-19 12:01:00 +0200

    shorter match()

--- src/core/match.ts
+++ src/core/match.ts
@@ -4,27 +4,21 @@ import { LLangAst, AstMap, isAtom, isLLangAst, isConst } from "./types.ts";
 
 export function match(template: LLangAst, f: LLangAst): AstMap | undefined {
 
-    if (template.type === 'is-a-formula' && f.type === 'is-a-formula') {
+    if (template.type === 'entity' && f.type === 'entity') {
+        return template.value === f.value ? deepMapOf() : undefined
+    } else if (template.type === 'variable' && f.type === 'variable') {
+        // return template.varType === f.varType ? deepMapOf([[template, f]]) : undefined // may undermatch in case of subtype/supertype relationships
+        return deepMapOf([[template, f]]) // overmatch to avoid having to check subtype/supertype relations????
+    } else if (
+        template.type === f.type ||
+        template instanceof Array && f instanceof Array
+    ) {
 
-        const m1 = match(template.subject, f.subject)
-        const m2 = match(template.object, f.object)
-        const m3 = match(template.after, f.after)
+        const templateT = template as { [x: string]: LLangAst }
+        const fT = f as { [x: string]: LLangAst }
 
-        return reduceMatchList([m1, m2, m3])
-
-    } else if (template.type === 'has-formula' && f.type === 'has-formula') {
-
-        const m1 = match(template.subject, f.subject)
-        const m2 = match(template.object, f.object)
-        const m3 = match(template.after, f.after)
-        const m4 = match(template.as, f.as)
-
-        return reduceMatchList([m1, m2, m3, m4])
-
-    } else if (template.type === 'generalized' && f.type === 'generalized') {
-
-        const templateKeys = Object.keys(template).filter(x => isLLangAst(template[x]))
-        const fKeys = Object.keys(f).filter(x => isLLangAst(f[x]))
+        const templateKeys = Object.keys(template).filter(x => isLLangAst(templateT[x]) || (templateT[x] instanceof Array))
+        const fKeys = Object.keys(f).filter(x => isLLangAst(fT[x]) || (fT[x] instanceof Array))
 
         if (templateKeys.length !== fKeys.length) {
             return undefined
@@ -32,8 +26,8 @@ export function match(template: LLangAst, f: LLangAst): AstMap | undefined {
 
         const ms = templateKeys.map(k => {
 
-            const v1 = template[k]
-            const v2 = f[k]
+            const v1 = templateT[k]
+            const v2 = fT[k]
 
             if (!v1 || !v2) return undefined
 
@@ -42,12 +36,6 @@ export function match(template: LLangAst, f: LLangAst): AstMap | undefined {
             return result
         })
 
-        return reduceMatchList(ms)
-
-    } else if (template.type === 'list-literal' && f.type === 'list-literal') {
-
-        if (template.value.length !== f.value.length) return undefined
-        const ms = template.value.map((x, i) => match(x, f.value[i]))
         return reduceMatchList(ms)
 
     } else if (template.type === 'list-pattern' && f.type === 'list-literal') {
@@ -62,31 +50,8 @@ export function match(template: LLangAst, f: LLangAst): AstMap | undefined {
 
         return reduceMatchList([m1, m2])
 
-    } else if (template.type === 'equality' && f.type === 'equality') {
-
-        const m1 = match(template.subject, f.subject)
-        const m2 = match(template.object, f.object)
-
-        return reduceMatchList([m1, m2])
-
-    } else if (template.type === 'entity' && f.type === 'entity') {
-        return template.value === f.value ? deepMapOf() : undefined
-
     } else if (template.type === 'anything' && isConst(f)) {
         return deepMapOf()
-
-    } else if (template.type === 'arbitrary-type' && f.type === 'arbitrary-type') {
-
-
-        const m1 = match(template.head, f.head)
-        const m2 = match(template.description, f.description)
-
-        return reduceMatchList([m1, m2])
-
-    } else if (template.type === 'existquant' && f.type === 'existquant') {
-
-        const m1 = match(template.value, f.value)
-        return m1
 
     } else if (template.type === 'arbitrary-type' && f.type === 'variable') {
 
@@ -101,22 +66,6 @@ export function match(template: LLangAst, f: LLangAst): AstMap | undefined {
     } else if (template.type === 'arbitrary-type' && isConst(f)) {
 
         return deepMapOf([[template, f]])
-
-
-    } else if (template.type === 'conjunction' && f.type === 'conjunction') {
-
-        const m1 = match(template.f1, f.f1)
-        const m2 = match(template.f2, f.f2)
-        const m11 = match(template.f1, f.f2)
-        const m22 = match(template.f2, f.f1)
-
-        return reduceMatchList([m1, m2]) ?? reduceMatchList([m11, m22])
-
-    } else if (template.type === 'variable' && f.type === 'variable') {
-
-        //TODO
-        // return template.varType === f.varType ? deepMapOf([[template, f]]) : undefined // may undermatch in case of subtype/supertype relationships
-        return deepMapOf([[template, f]]) // overmatch to avoid having to check subtype/supertype relations????
 
     } else if (template.type === 'variable' && isAtom(f)) {
         return deepMapOf([[template, f]])
