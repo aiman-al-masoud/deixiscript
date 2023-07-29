eeb25238c6ddf087cc19080462744f6f397a743f
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-07-29 16:08:34 +0200

    ...

--- src/core/decompress.ts
+++ src/core/decompress.ts
@@ -5,13 +5,13 @@ import { LLangAst, pointsToThings } from "./types.ts";
 export function decompress<T extends LLangAst>(ast: T): T
 export function decompress(ast: LLangAst): LLangAst {
 
-    const conj = findAsts(ast, 'conjunction').at(0) ?? findAsts(ast, 'disjunction').at(0)
+    const logiConn = findAsts(ast, 'conjunction').at(0) ?? findAsts(ast, 'disjunction').at(0)
 
-    if (conj && pointsToThings(conj.f1) && pointsToThings(conj.f2)) {
-        const withF1 = subst(ast, [conj, conj.f1])
-        const withF2 = subst(ast, [conj, conj.f2])
+    if (logiConn && pointsToThings(logiConn.f1) && pointsToThings(logiConn.f2)) {
+        const withF1 = subst(ast, [logiConn, logiConn.f1])
+        const withF2 = subst(ast, [logiConn, logiConn.f2])
         return {
-            type: ast.type === 'negation' ? opposite(conj.type) : conj.type, // de morgan
+            type: ast.type === 'negation' ? opposite(logiConn.type) : logiConn.type, // de morgan
             f1: decompress(withF1),
             f2: decompress(withF2),
         }
