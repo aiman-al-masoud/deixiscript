95bcd4a1bf40146498b82f254f391f0ad01ebaea
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-04 19:17:12 +0200

    ...

--- src/core/getParts.ts
+++ src/core/getParts.ts
@@ -3,26 +3,19 @@ import { ask } from "./ask.ts";
 import { $ } from "./exp-builder.ts";
 import { WmAtom, KnowledgeBase, conceptsOf, isHasSentence, isTruthy } from "./types.ts";
 
-
 export function getParts(concept: WmAtom, kb: KnowledgeBase): WmAtom[] {
-    const parts = getAllParts(concept, kb)
-    return uniq(parts)
-}
-
-function getAllParts(concept: WmAtom, kb: KnowledgeBase): WmAtom[] {
 
     const supers = conceptsOf(concept, kb)
 
     const parts = kb.wm
         .filter(isHasSentence)
         .filter(x => x[0] === concept)
-        .filter(x => isTruthy(ask($(x[1]).isa('only-one-annotation').isNotTheCase.$, kb).result))
         .filter(x => isTruthy(ask($(x[1]).isa('number-restriction').isNotTheCase.$, kb).result))
         .filter(x => isTruthy(ask($(x[1]).isa('mutex-concepts-annotation').isNotTheCase.$, kb).result))
         .filter(x => isTruthy(ask($(x[1]).isa('mutex-annotation').isNotTheCase.$, kb).result))
         .map(x => x[2])
 
-    const all = supers.filter(x => x !== concept).flatMap(x => getAllParts(x, kb)).concat(parts)
+    const all = supers.filter(x => x !== concept).flatMap(x => getParts(x, kb)).concat(parts)
     return uniq(all)
 
 }
