54f08b2104430a80eae2278219f893ecc6fd6a2a
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-08 12:08:13 +0200

    ...

--- src/core/evaluate.ts
+++ src/core/evaluate.ts
@@ -1,7 +1,7 @@
 import { ask } from "./ask.ts";
 import { $ } from "./exp-builder.ts";
 import { tell } from "./tell.ts";
-import { Atom, KnowledgeBase, LLangAst, WorldModel } from "./types.ts";
+import { KnowledgeBase, LLangAst, WorldModel } from "./types.ts";
 
 export function evaluate(ast: LLangAst, knowledgeBase: KnowledgeBase): {
     kb: KnowledgeBase,
