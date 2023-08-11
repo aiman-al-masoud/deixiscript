6212f1e25a78f851431fb6fdca369a1a753929d1
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-11 17:58:16 +0200

    ...

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -3,5 +3,5 @@
  * To be used instead of deno assert() in normal code (outside tests).
  */
 export function assert(expr: unknown, msg?: string): asserts expr {
-    if (!expr) throw new Error(msg ?? '')
+    if (!expr) throw new Error('Assertion Error: ' + msg ?? '')
 }
