a8ab7ff699c00f0227dbaf5d494dc3e7d040fe42
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-13 18:13:18 +0200

    ...

--- src/parser/parse.ts
+++ src/parser/parse.ts
@@ -7,11 +7,7 @@ export function parse(
     kb: KnowledgeBase,
 ): LLangAst {
 
-    // console.log('ast=', ast)
     const when = definitionOf(ast, kb)
-    // console.log('when=', when)
-    // console.log('----------------')
-
     if (!when) return ast
 
     const ast2 = mapAsts(when, x => parse(x, kb))
