f4cc72ea90abd0e85f545b57cae09630992f3e53
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-13 09:38:47 +0200

    ...

--- src/utils/isNotNullish.ts
+++ src/utils/isNotNullish.ts
@@ -1,7 +1,3 @@
 export function isNotNullish<T>(x: T | undefined | null): x is T {
     return x !== undefined && x !== null
 }
-
-export function isNullish<T>(x: T | undefined | null): x is undefined | null {
-    return !isNotNullish(x)
-}
