de12a002322f2a1c4dd2e8e51821ce574a1c1e91
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-13 23:35:41 +0200

    ...

--- src/core/mapAsts.ts
+++ src/core/mapAsts.ts
@@ -3,26 +3,26 @@ import { LLangAst, isLLangAst } from "./types.ts";
 export function mapAsts(
     ast: LLangAst,
     fn: (x: LLangAst) => LLangAst,
-    args?: { top?: boolean, lvl?: number },
+    params?: { top?: boolean, lvl?: number },
 ): LLangAst {
 
-    const pars = {
+    const args = {
         top: true,
         lvl: 0,
-        ...args,
+        ...params,
     }
 
     const entries = Object.entries(ast)
         .filter((e): e is [string, LLangAst] => isLLangAst(e[1]))
 
     const newEntries =
-        entries.map(e => [e[0], mapAsts(e[1], fn, { ...pars, lvl: pars.lvl + 1 })])
+        entries.map(e => [e[0], mapAsts(e[1], fn, { ...args, lvl: args.lvl + 1 })])
 
     const nucleus = {
         ...ast,
         ...Object.fromEntries(newEntries),
     }
 
-    return pars.lvl !== 0 || pars.top ? fn(nucleus) : nucleus
+    return args.lvl !== 0 || args.top ? fn(nucleus) : nucleus
 
 }
