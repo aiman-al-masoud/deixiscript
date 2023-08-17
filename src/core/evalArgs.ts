9fff38548ef4bb8944c0321b72783878346d5a00
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-17 18:52:10 +0200

    ...

--- src/core/evalArgs.ts
+++ src/core/evalArgs.ts
@@ -1,5 +1,5 @@
 import { valueIs } from "../utils/valueIs.ts"
-import { ask } from "./ask.ts"
+import { evaluate } from "./evaluate.ts"
 import { LLangAst, KnowledgeBase, isLLangAst } from "./types.ts"
 
 
@@ -10,8 +10,8 @@ export function evalArgs(ast: LLangAst, kb: KnowledgeBase) {
 
     case 'complement':
       {
-        const { result: complementName, kb: kb1 } = ask(ast.complementName, kb)
-        const { result: complement, kb: kb2 } = ask(ast.complement, kb1)
+        const { result: complementName, kb: kb1 } = evaluate(ast.complementName, kb)
+        const { result: complement, kb: kb2 } = evaluate(ast.complement, kb1)
         const rast = { ...ast, complement, complementName }
         return { rast, kb: kb2 }
       }
@@ -35,7 +35,7 @@ export function evalArgs(ast: LLangAst, kb: KnowledgeBase) {
       {
 
         const res = Object.entries(ast).filter(valueIs(isLLangAst)).reduce((a, e) => {
-          const r = e[1].type === 'complement' || e[1].type === 'generalized' ? { result: e[1], kb } : ask(e[1], a.kb)
+          const r = e[1].type === 'complement' || e[1].type === 'generalized' ? { result: e[1], kb } : evaluate(e[1], a.kb)
           return { rast: { ...a.rast, [e[0]]: r.result }, kb: r.kb }
         }, { rast: ast, kb: kb })
         return res
