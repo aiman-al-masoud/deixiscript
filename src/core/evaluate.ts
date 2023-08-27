e99bd4b0cd54ac65265a7958dc92995f701e07d1
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-27 16:12:31 +0200

    ...

--- src/core/evaluate.ts
+++ src/core/evaluate.ts
@@ -2,8 +2,6 @@ import { ask } from "./ask.ts";
 import { decompress } from "./decompress.ts";
 import { definitionOf } from "./definitionOf.ts";
 import { evalArgs } from "./evalArgs.ts";
-// import { $ } from "./exp-builder.ts";
-// import { findAsts } from "./findAsts.ts";
 import { removeImplicit } from "./removeImplicit.ts";
 import { tell } from "./tell.ts";
 import { KnowledgeBase, LLangAst, WorldModel } from "./types.ts";
@@ -14,23 +12,16 @@ export function evaluate(ast: LLangAst, kb: KnowledgeBase, args: { asIs: boolean
     additions: WorldModel,
     eliminations: WorldModel,
 } {
-    const x = ast.type === 'command' ? ast.f1 : ast
+    const ast1 = ast.type === 'command' ? ast.f1 : ast
     const f = ast.type === 'command' ? tell : ask
     if (args.asIs) return { additions: [], eliminations: [], ...f(ast, kb) }
-    return execAst(x, kb, f)
-}
-
-function execAst(ast: LLangAst, kb: KnowledgeBase, f: typeof tell | typeof ask) {
-
-    const { ast: rast3, kb: kb1 } = translate(ast, kb)
-
+    const { ast: ast2, kb: kb1 } = translate(ast1, kb)
     return {
         additions: [],
         eliminations: [],
-        ...f(rast3, kb1),
+        ...f(ast2, kb1),
     }
 }
-
 
 function translate(ast: LLangAst, kb: KnowledgeBase): { ast: LLangAst, kb: KnowledgeBase } {
     const { rast, kb: kb1 } = evalArgs(ast, kb)
