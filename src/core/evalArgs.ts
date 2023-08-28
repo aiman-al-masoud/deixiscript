cf98994eeaa218f6a87e8a634fcdbaac4221bd20
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-08-28 21:21:48 +0200

    evalArgs...

--- src/core/evalArgs.ts
+++ src/core/evalArgs.ts
@@ -7,14 +7,6 @@ export function evalArgs<T extends LLangAst>(ast: T, kb: KnowledgeBase): { rast:
 export function evalArgs(ast: LLangAst, kb: KnowledgeBase): { rast: LLangAst, kb: KnowledgeBase } {
 
   switch (ast.type) {
-
-    case 'complement':
-      {
-        // const { result: complementName, kb: kb1 } = evaluate(ast.complementName, kb)
-        const { result: complement, kb: kb2 } = evaluate(ast.complement, kb /* kb1 */)
-        const rast = { ...ast, complement, /* complementName */ }
-        return { rast, kb: kb2 }
-      }
     case 'if-else':
     case 'conjunction':
     case 'disjunction':
@@ -31,29 +23,23 @@ export function evalArgs(ast: LLangAst, kb: KnowledgeBase): { rast: LLangAst, kb
     case "when-derivation-clause":
     case "implicit-reference":
     case "after-derivation-clause":
+    case "command":
       return { rast: ast, kb }
-
-    // case "is-a-formula":
-      // {
-      //   const { result:object, kb:kb1 } = evaluate(ast.object, kb)
-      //   const { result:subject, kb:kb2 } = evaluate(ast.subject, kb1)
-      //   const rast= {...ast,subject,object}
-      //   return {rast, kb:kb2}
-      // }
+    case 'complement':
     case "is-a-formula":
     case "has-formula":
     case "math-expression":
     case "generalized":
+
       {
-        const res = Object.entries(ast).filter(valueIs(isLLangAst)).reduce((a, e) => {
+        const order = ['complementName', 'complement', 'phrase']
+        const res = Object.entries(ast).filter(valueIs(isLLangAst)).sort((b, a) => order.indexOf(a[0]) - order.indexOf(b[0])).reduce((a, e) => {
           if (!pointsToThings(e[1])) return a
           const r = evaluate(e[1], a.kb)
           return { rast: { ...a.rast, [e[0]]: r.result }, kb: r.kb }
         }, { rast: ast, kb: kb })
         return res
       }
-    case "command":
-      throw new Error(``)
 
   }
 
