6d3e54040aa16baa996c5b9578b40e8e41c3fc94
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-09-12 13:25:18 +0200

    ...

--- src/matchAst.py
+++ src/matchAst.py
@@ -1,5 +1,5 @@
-from expbuilder import e
-from language import KnowledgeBase, NounPhrase
+from expbuilder import e, it_is_false_that
+from language import Ast, KnowledgeBase, NounPhrase
 
 
 def matchNp(template:NounPhrase, ast:NounPhrase, kb:KnowledgeBase):
@@ -7,3 +7,11 @@ def matchNp(template:NounPhrase, ast:NounPhrase, kb:KnowledgeBase):
     r2 = e(ast).ask(r1.kb) # TODO: only one (latest) result!! cardinality
     return r1.head == r2.head
 
+#
+# Two sentences are synonymous in a context kb, when
+# their truth values co-vary in kb.
+#
+def matchFm(template:Ast, ast:Ast, kb:KnowledgeBase):
+    r1 = e(template).tell(kb)
+    r2 = it_is_false_that(template).tell(kb)
+    return e(ast).get(r1.kb) and it_is_false_that(ast).get(r2.kb)
