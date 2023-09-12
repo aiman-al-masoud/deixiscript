d8c5deff45c6294bffe2d20d8582d58ba81ec844
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-09-12 12:13:38 +0200

    ...

--- src/evaluate.py
+++ src/evaluate.py
@@ -7,7 +7,7 @@ def evaluate(ast:Ast, kb:KnowledgeBase)->Result:
     TOP LEVEL ONLY!
     NOT supposed to be called by any other function! 
     """
-    # TODO: check for match in analytic derivaton clauses
+    # TODO: check for definitions in analytic derivaton clauses
     x1 = expandNegations(ast)
     x2 = expandCommands(x1)
     x3 = removeImplicit(x2, kb)
