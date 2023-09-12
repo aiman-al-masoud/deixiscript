680c2805535c4e5cfd47dea7ea08ee7fcbae8717
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-09-12 20:09:11 +0200

    ...

--- src/matchAst.py
+++ src/matchAst.py
@@ -6,7 +6,7 @@ from language import Ast, KnowledgeBase, NounPhrase
 # Two expressions are synonymous in a context kb, when
 # their truth values (or referents) co-vary in kb.
 #
-def matchFm(template:Ast, ast:Ast, kb:KnowledgeBase):
+def matchAst(template:Ast, ast:Ast, kb:KnowledgeBase):
     r1 = e(template).tell(kb)
     r2 = it_is_false_that(template).tell(kb)
     return e(ast).get(r1.kb)==r1.head and it_is_false_that(ast).get(r2.kb)==r2.head
