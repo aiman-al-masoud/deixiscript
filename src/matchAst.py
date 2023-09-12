423eeddf03524165bbb258bf51fc94e14bea4cda
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-09-12 12:25:06 +0200

    ...

--- src/matchAst.py
+++ src/matchAst.py
@@ -4,6 +4,6 @@ from language import KnowledgeBase, NounPhrase
 
 def matchNp(template:NounPhrase, ast:NounPhrase, kb:KnowledgeBase):
     r1 = e(template).tell(kb)
-    r2 = e(ast).ask(r1.kb)
+    r2 = e(ast).ask(r1.kb) # TODO: only one (latest) result!! cardinality
     return r1.head == r2.head
 
