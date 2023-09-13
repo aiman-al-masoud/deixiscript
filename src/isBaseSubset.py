1c53f79204e3cd13dcbd41f905c17b052699bd1c
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-09-13 16:43:18 +0200

    ...

---
+++ src/isBaseSubset.py
@@ -0,0 +1,8 @@
+from language import Ast
+
+
+def isBaseSubset(ast:Ast):
+    # no implicit references, only explicit
+    # everything is decompressed
+    # verb sentences don't cotain negations or commands
+    raise Exception('')
