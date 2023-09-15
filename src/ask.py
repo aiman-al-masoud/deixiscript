e79683ae3043d4b7e6a22b485d4922a44f4c46ca
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-09-15 16:33:02 +0200

    ...

--- src/ask.py
+++ src/ask.py
@@ -96,8 +96,7 @@ def tell(ast:Ast, kb:KnowledgeBase)->Result:
         case Negation(v):
             r1 = tell(v, kb)
             kb1 = kb.subWm(r1.addition).updateDD(r1.kb.dd)
-            return Result(r1.head, kb1) 
-            # return Result(tuple(), kb1)
+            return Result(r1.head, kb1) # or head=tuple()?
         case BinExp('and', l, r):
             r1 = tell(l, kb)
             r2 = tell(r, r1.kb)
