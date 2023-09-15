93e31449b9d0a54e059ee3915c570e3f607cc859
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-09-16 01:20:25 +0200

    ...

--- src/findAsts.py
+++ src/findAsts.py
@@ -1,7 +1,7 @@
 from typing import Callable, Tuple
 from language import Ast, Explicit
 
-def findAsts(ast:Ast, fn:Callable[[Ast],bool])->Tuple[Ast]:
+def findAsts(ast:Ast, fn:Callable[[Ast],bool])->Tuple[Ast, ...]:
 
     if fn(ast): return (ast,)
 
@@ -9,4 +9,3 @@ def findAsts(ast:Ast, fn:Callable[[Ast],bool])->Tuple[Ast]:
     if isinstance(ast, Explicit): return tuple()
 
     return tuple(y for x in ast.__dict__.values() for y in findAsts(x, fn))
-
