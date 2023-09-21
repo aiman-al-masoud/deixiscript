e0a1ecc71e8fe8bbd5856569e96049223a0c0df7
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-09-21 16:27:44 +0200

    ...

--- src/matchAst.py
+++ src/matchAst.py
@@ -38,8 +38,8 @@ def matchAst(generic:Ast, specific:Ast, kb:KnowledgeBase=KnowledgeBase.empty):
     without1 = it_is_false_that(generic).tell(kb)
     without2 = e(specific).get(without1.kb)
 
-    return agree(with1.head, with2) and not agree(without1.head, without2)
+    return with2 and not without2
+    # return agree(with1.head, with2) and not agree(without1.head, without2)
 
-
-def agree(ast1:Ast, ast2:Ast):
-    return  bool(ast1) == bool(ast2)
+# def agree(ast1:Ast, ast2:Ast):
+#     return  bool(ast1) == bool(ast2)
