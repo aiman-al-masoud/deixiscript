450d0f4658e993244a2376b4f5339392f165f03d
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-09-25 09:36:33 +0200

    ...

--- src/parser/parse.py
+++ src/parser/parse.py
@@ -1,24 +1,24 @@
-from typing import Dict, List
-from match import Lit, Map, match
+from typing import List
+from match import Map, match
 from metalang import Derivation
 
 
-def parse(ds:List[Derivation], toks:List[Lit]|Dict[str, object]|object):
+def parse(ds:List[Derivation], toks:object):
 
     match toks:
         case list(xs):
             r1 = [match(d.pat, xs) for d in ds]
             r2 = zip(r1, ds)
-            r3 = next((subst(x[1].definition, x[0]) for x in r2 if x[0] is not None), None)
+            r3 = next((subst(d.definition, m) for m,d in r2 if m is not None), None)
             if r3 is None: return toks
             r4 = parse(ds, r3)
             return r4
-        case dict(d): # TODO: any object
-            return {k:parse(ds, v) for k,v in d.items()}
         case str(x)|int(x)|float(x):
             return x
+        case dict(d): # TODO: any object
+            return {k:parse(ds, v) for k,v in d.items()}
         case _:
-            raise Exception('')
+            raise Exception('parse', toks)
 
 def subst(ast:object, map:Map):
     match ast:
