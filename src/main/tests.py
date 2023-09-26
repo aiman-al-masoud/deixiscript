356805f9f968bdd22c9170cb1059dee519e58f9a
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-09-26 17:48:20 +0200

    ...

--- src/main/tests.py
+++ src/main/tests.py
@@ -1,15 +1,16 @@
 from typing import Literal
 from parser.parse import parse
 from parser.metalang import L,V,D
-from core.expbuilder import e
+from core.expbuilder import does, e
 from core.language import BinExp
 
+# TODO: implicit and idiomatic by default
 
 ds = [
     D([V('x')], 'x'),
-    D([L('h'), 'which', L('w')],{'head':'h', 'which':'w'}),
+    D([L('h'), 'which', L('w')],e('h').which('w').e),
     D([L('l'), V('op', Literal['and', 'or']), L('r')], BinExp('op', 'l', 'r')),
-    D([L('s', default=''), 'does', V('v'), L('o', default=''),],{'subject':'s', 'verb':'v', 'object':'o' }),
+    D([L('s', default=''), 'does', V('v'), L('o', default=False),], e('s').does('v')._('o').e),
     D(['(', L('x'), ')'], 'x')
 ]
 
@@ -21,19 +22,19 @@ def test_g1():
 
 def test_g2():
     x = parse(ds, ['cat', 'does', 'run'])
-    assert x == {'subject':'cat', 'verb':'run', 'object':''}
+    assert x == e('cat').does('run').e
 
 def test_g3():
     x = parse(ds, ['does', 'run'])
-    assert x == {'subject':'', 'verb':'run', 'object':''}
+    assert x == does('run').e
     
 def test_g4():
-    x = parse(ds, ['cat', 'which', 'does', 'run'])
-    assert x == {'head':'cat', 'which':{'subject':'', 'verb':'run', 'object':''}}
+    x = parse(ds, ['cat', 'does', 'eat','mouse'])
+    assert x == e('cat').does('eat')._('mouse').e
 
 def test_g5():
-    x = parse(ds, ['cat', 'does', 'eat','mouse'])
-    assert x == {'subject':'cat', 'verb':'eat', 'object':'mouse'}
+    x = parse(ds, ['cat', 'which', 'does', 'run'])
+    assert x == e('cat').which(does('run')).e
 
 # def test_g6():
 #     x = parse(ds, ['cat', 'which', 'does', 'be', 'red', 'does', 'eat','mouse'])
