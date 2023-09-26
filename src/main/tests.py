f538160cc4058e52c679719313555d6e81ce469d
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-09-26 17:58:21 +0200

    yeah

--- src/main/tests.py
+++ src/main/tests.py
@@ -1,15 +1,20 @@
 from typing import Literal
+from core.normalized import isNounPhrasish
 from parser.parse import parse
 from parser.metalang import L,V,D
 from core.expbuilder import does, e
 from core.language import BinExp
+# from core.normalized import isNounPhrasish
+
+
+
 
 # TODO: implicit and idiomatic by default
 
 ds = [
     D([V('x')], 'x'),
     D([L('h'), 'which', L('w')],e('h').which('w').e),
-    D([L('l'), V('op', Literal['and', 'or']), L('r')], BinExp('op', 'l', 'r')),
+    D([L('l', isNounPhrasish), V('op', Literal['and', 'or']), L('r', isNounPhrasish)], BinExp('op', 'l', 'r')),
     D([L('s', default=''), 'does', V('v'), L('o', default=False),], e('s').does('v')._('o').e),
     D(['(', L('x'), ')'], 'x')
 ]
@@ -40,9 +45,9 @@ def test_g5():
 #     x = parse(ds, ['cat', 'which', 'does', 'be', 'red', 'does', 'eat','mouse'])
 #     # WRONG!
 
-# def test_g7():
-#   x = parse(ds, ['cat', 'and', 'dog', 'does', 'run'])
-#   #WRONG!
+def test_g7():
+  x = parse(ds, ['cat', 'and', 'dog', 'does', 'run'])
+  assert x == e('cat').and_('dog').does('run').e
 
 def test_g8():
    x = parse(ds, ['(', 1, ')'])
