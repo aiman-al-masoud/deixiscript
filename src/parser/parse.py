85078cee118bae704fa0b6435a6274c3f8884ba7
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-13 16:45:30 +0200

    ...

--- src/parser/parse.py
+++ src/parser/parse.py
@@ -5,8 +5,11 @@ from core.language import BinExp, Command, Negation, SimpleSentence, Which
 
 preps  = {'to', 'on', 'as'}
 binops = {'and', 'or'}
-verb_markers = {'does'}
+verb_markers = {'does', 'do'}
 relativizers = {'which'}
+whens = {'when'}
+afters = {'after'}
+negations = {'not'}
 
 def parse(toks:List[str|int|float]):
 
@@ -18,15 +21,15 @@ def parse(toks:List[str|int|float]):
             return Command(parse(xs))
         case [*xs, '?']:
             return parse(xs)
-        case _ if ps:=splitBy(toks, {'when'}):
+        case _ if ps:=splitBy(toks, whens):
             return e(parse(ps[0])).when(parse(ps[2])).e
-        case _ if ps:=splitBy(toks, {'after'}):
+        case _ if ps:=splitBy(toks, afters):
             return e(parse(ps[0])).after(parse(ps[2])).e
         case _ if ps:=splitBy(toks, relativizers):
             return Which(parse(ps[0]), parse(ps[2]))
         case _ if ps:=splitBy(toks, verb_markers):
             subject     = parse(ps[0])
-            negation    = toks[ps[1][1]+1]=='not'
+            negation    = toks[ps[1][1]+1] in negations
             verb        = toks[ps[1][1]+2] if negation else toks[ps[1][1]+1]
             rest        = toks[toks.index(verb)+1:]
             endObject   = next((i for i, x in enumerate(rest) if x in preps), len(toks))
