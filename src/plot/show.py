dca76f3abf100ef7dbd88351f75716d5252c07e7
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-13 12:02:51 +0200

    ...

--- src/plot/show.py
+++ src/plot/show.py
@@ -1,15 +1,15 @@
-import sys
-import os
-path = os.path.split(os.path.split(__file__)[0])[0]
-sys.path.append(path)
 from core.language import Ast
 from core.KnowledgeBase import WorldModel
-from core.expbuilder import the
 
 def show(wm:WorldModel):
     from graphviz import Source
     source = graphvizied(wm)
     Source(source, filename='tmp.gv', format='png').view()
+
+def save_png(wm:WorldModel):
+    from graphviz import Source
+    source = graphvizied(wm)
+    Source(source, filename='tmp.gv', format='png').render()
 
 def graphvizied(wm:WorldModel):
     from functools import reduce
@@ -20,13 +20,3 @@ def graphvizied(wm:WorldModel):
 
 def ast_to_graphviz(ast:Ast):
     raise Exception()
-
-def test_c41():
-    kb0 = the('horse').tell()
-    kb1 = the('man').tell(kb0)
-    kb2 = the('man').does('ride').on(the('horse')).tell(kb1)
-    kb3 = the('horse').does('move').tell(kb2)
-    kb4 = the('man').does('yell').tell(kb3)
-    show(kb4.wm)
- 
-test_c41()
---
+++ src/plot/tests.py
@@ -0,0 +1,14 @@
+
+from core.expbuilder import the
+from plot.show import save_png, show
+
+def tezt_c41():
+    kb0 = the('horse').tell()
+    kb1 = the('man').tell(kb0)
+    kb2 = the('man').does('ride').on(the('horse')).tell(kb1)
+    kb3 = the('horse').does('move').tell(kb2)
+    kb4 = the('man').does('yell').tell(kb3)
+    show(kb4.wm)
+    # save_png(kb4.wm)
+ 
+# tezt_c41()
