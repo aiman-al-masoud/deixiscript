b640e0ec4de129e04d3264b3b9e604d272b97c22
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-20 22:29:10 +0200

    ...

--- src/parse/parse.py
+++ src/parse/parse.py
@@ -1,6 +1,7 @@
 from lark import Lark
 
-grammar = open('grammar.lark').read()
-parser = Lark(grammar, start='ast', ambiguity='explicit')
-
+grammar = open('./grammar.lark').read()
+parser = Lark(grammar, start='ast') #  ambiguity='explicit'
+x = parser.parse('capra which does be red does not jump')
+# print(x.pretty())
 # transform...
