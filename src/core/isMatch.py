0699eb502f80933f8023c70a23aadcad90d7afcb
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-21 12:32:31 +0200

    ...

--- src/core/isMatch.py
+++ src/core/isMatch.py
@@ -6,15 +6,13 @@ def isMatch(sub:Ast, sup:Ast)->bool:
     match sub, sup:
         case str()|int()|float(), str()|int()|float():
             return sub==sup
+
         case Implicit(), Implicit():
-            
             # if sup.card > sub.card: return False
             return isMatch(sub.head, sup.head) and isMatch(sub.which, sup.which)
 
         case SimpleSentence(), SimpleSentence():
-
             if sup.verb != sub.verb: return False
-            
             return isMatch(sub.subject, sup.subject)                   and\
                    (not sup.object or isMatch(sub.object, sup.object)) and\
                    (not sup.as_    or isMatch(sub.as_, sup.as_))       and\
@@ -33,20 +31,18 @@ def isMatch(sub:Ast, sup:Ast)->bool:
         case SimpleSentence(), BinExp(op='or'):
             return isMatch(sub, sup.left) or isMatch(sub, sup.right)
 
-        # case BinExp(op='and'|'or'), BinExp(op='and'|'or'):
-
-        #     # wrong (and)
-        #     return isMatch(sup.left,  sub.left) and\
-        #     isMatch(sup.right, sub.right)       and\
-        #     isMatch(sup.left,  sub.right)       and\
-        #     isMatch(sup.right, sub.left)
+        case BinExp(op='and'|'or'), BinExp(op='and'|'or'):
+            raise Exception()
 
         case Def(definendum=d1), Def(definendum=d2):
             return isMatch(d1, d2)
+
         case Law(cause=c1), Law(cause=c2):
             return isMatch(c1, c2)
+
         case _, True:
             return True
+
         case _:
             return False
             # raise Exception(sub, sup)
