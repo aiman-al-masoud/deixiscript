cc47670443a0b3d108cbdee1d2f93f853f3b2704
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-23 02:01:01 +0200

    ...

--- src/core/language.py
+++ src/core/language.py
@@ -4,7 +4,7 @@ if TYPE_CHECKING:
     from core.KB import KB
 
 class Ast:
-    def copy(self, **kwargs)->'Ast': raise Exception()
+    def copy(self, **kwargs:'Ast')->'Ast': raise Exception()
     def eval(self, kb:'KB')->'KB': raise Exception()
     def unroll(self)->Sequence['Ast']: raise Exception()
     def isMatch(self, sub:'Ast')->Optional[Dict['Ast', 'Ast']]: raise Exception()
