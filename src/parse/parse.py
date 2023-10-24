79228803f539c33ac238aa2b9cf4fb598ae42e40
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-24 02:46:37 +0200

    ...

--- src/parse/parse.py
+++ src/parse/parse.py
@@ -1,6 +1,8 @@
 import os
 from lark import Lark
-from parse.transform import toDict
+
+from parse.transform import ToDict
+# from parse.transform import toDict
 
 grammar_path=os.path.join(os.path.split(__file__)[0], 'grammar2.lark')
 grammar = open(grammar_path).read()
@@ -11,13 +13,17 @@ parser = Lark(grammar, start='ast', ambiguity='explicit')
 # print(x)
 
 parser = Lark(grammar, start='ast', ambiguity='explicit')
-x = parser.parse('cat does eat on table as hungry?')
+x = parser.parse('1 cat does run')
+
+y = ToDict().transform(x)
+print(y)
+
 # x = parser.parse('the 1 cat')
 # x = parser.parse('the 1 cat does run')
 # x = parser.parse('the cat')
 # print(x)
-realtypes={'noun', 'relative', 'simple_sentence', 'compound_sentence'}
-print(toDict(x, realtypes))
+# realtypes={'noun', 'relative', 'simple_sentence', 'compound_sentence'}
+# print(toDict(x, realtypes))
 # y = BurufTransformer().transform(x)
 # print(y)
 # y = transform(x)
--- src/parse/transform.py
+++ src/parse/transform.py
@@ -21,32 +21,91 @@ from lark import Lark, Token, Transformer, Tree
 from core.expbuilder import every
 
 
-def toDict(st:Tree|Token|List[Tree], realtypes:Set[str]=set()):
-    match st:
-        case Token():
-            if st.isnumeric(): return int(st)
-            return str(st)
-        case Tree():
-            mytype=st.data.value
-            children=toDict(st.children, realtypes)
-            if mytype in realtypes: return {'type':mytype, **children}
-            return {mytype:children}
-        case [*xs]:
-            children= [toDict(x, realtypes) for x in xs]
-            if len(children)==1: return children[0]
+# def toDict(st:Tree|Token|List[Tree], realtypes:Set[str]=set()):
+#     match st:
+#         case Token():
+#             if st.isnumeric(): return int(st)
+#             return str(st)
+#         case Tree():
+#             mytype=st.data.value
+#             children=toDict(st.children, realtypes)
+#             if mytype in realtypes: return {'type':mytype, **children}
+#             return {mytype:children}
+#         case [*xs]:
+#             children= [toDict(x, realtypes) for x in xs]
+#             if len(children)==1: return children[0]
 
-            big = {}
+#             big = {}
 
-            for c in children:
+#             for c in children:
                 
-                if comm:=c.keys()&big:
-                    for k in comm: 
-                        big[k] = [big[k], c[k]]
-                    continue
+#                 if comm:=c.keys()&big:
+#                     for k in comm: 
+#                         big[k] = [big[k], c[k]]
+#                     continue
 
-                big = {**big, **c}
+#                 big = {**big, **c}
 
-            return big
+#             return big
             
+# realtypes={'noun', 'relative', 'simple_sentence', 'compound_sentence'}
 
+class ToDict(Transformer):
+
+    def NUMBER(self, children):
+        return int(children)
+
+    def NAME(self, children):
+        return str(children)
+
+    def ord(self, children):
+        return {'ord': children}
+
+    def card(self, children):
+        return {'card': children[0] if len(children)==1 else children }
+
+    def negation(self, children):
+        return {'negation':children}
+    
+    def noun_head(self, children):
+        return {'noun_head':children}
+
+    def relative_head(self, children):
+        return {'relative_head':children}
+
+    def subordinate(self, children):
+        return {'subordinate':children}
+
+    def noun(self, children):
+        return {'type':'noun', **reduce(lambda a,b: {**a, **b}, children)}
+    
+    def complement_head(self, children):
+        return {'complement_head':children}
+
+    def preposition(self, children):
+        return {'preposition':children}
+
+    def complement(self, children):
+        return {'type':'complement', **reduce(lambda a,b: {**a, **b}, children)}
+
+    def cmd(self, children):
+        return {'cmd': True}
+
+    def relative(self, children):
+        return {'type':'relative', **reduce(lambda a,b: {**a, **b}, children)}
+
+    def subject(self, children):
+        return {'subject': children}
+
+    def object(self, children):
+        return {'object': children}
+
+    def verb(self, children):
+        return {'verb': children}
+
+    def simple_sentence(self, children):        
+        return {'type':'simple_sentence', **reduce(lambda a,b: {**a, **b}, children)}
+
+    def compound_sentence(self, children):
+        pass
 
