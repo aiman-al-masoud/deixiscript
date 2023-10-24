8726620e01e3d36bd88c00a975782525eb983ec0
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-24 11:08:31 +0200

    ...

--- src/parse/transform.py
+++ src/parse/transform.py
@@ -1,115 +1,100 @@
-
-# from typing import List
-# from lark import Lark, Transformer, Tree
-
-# from core.expbuilder import e
-
-# class BurufTransformer(Transformer):
-
-#     def NUMBER(self, tok):
-#         return tok.update(value=int(tok))
-
-#     def noun(self, children:List[Tree]):
-#         print('children=', children)
-
-#         return e(1)
-
 from functools import reduce
-from typing import List, Set
-from lark import Lark, Token, Transformer, Tree
-
-from core.expbuilder import every
-
-
-# def toDict(st:Tree|Token|List[Tree], realtypes:Set[str]=set()):
-#     match st:
-#         case Token():
-#             if st.isnumeric(): return int(st)
-#             return str(st)
-#         case Tree():
-#             mytype=st.data.value
-#             children=toDict(st.children, realtypes)
-#             if mytype in realtypes: return {'type':mytype, **children}
-#             return {mytype:children}
-#         case [*xs]:
-#             children= [toDict(x, realtypes) for x in xs]
-#             if len(children)==1: return children[0]
-
-#             big = {}
-
-#             for c in children:
-                
-#                 if comm:=c.keys()&big:
-#                     for k in comm: 
-#                         big[k] = [big[k], c[k]]
-#                     continue
-
-#                 big = {**big, **c}
-
-#             return big
-            
-# realtypes={'noun', 'relative', 'simple_sentence', 'compound_sentence'}
+from lark import Transformer
+from core.Law import Law
+from core.BinExp import BinExp
+from core.Def import Def
+from core.Implicit import Implicit
+from core.Int import Int
+from core.SimpleSentence import SimpleSentence
+from core.Str import Str
 
 class ToDict(Transformer):
 
     def NUMBER(self, children):
-        return int(children)
+        return Int(children)
 
     def NAME(self, children):
-        return str(children)
+        return Str(children)
 
     def PREPOSITION(self, children):
-        return str(children)
+        return Str(children)
+
+    def BINOP(self, children):
+        return Str(children)
 
     def ord(self, children):
-        return {'ord': children}
+        return {'ord': children[0]}
 
     def card(self, children):
-        return {'card': children[0] if len(children)==1 else children }
+        return {'card': children[0]}
 
-    def negation(self, children):
-        return {'negation':children}
-    
-    def noun_head(self, children):
-        return {'noun_head':children}
+    def negation(self, _):
+        return {'negation': Int(1)}
 
-    def relative_head(self, children):
-        return {'relative_head':children}
+    def cmd(self, _):
+        return {'cmd': Int(1)}
 
-    def subordinate(self, children):
-        return {'subordinate':children}
-
-    def noun(self, children):
-        return {'type':'noun', **reduce(lambda a,b: {**a, **b}, children)}
-    
-    def complement_head(self, children):
-        return {'complement_head':children}
-
-    def preposition(self, children):
-        return {'preposition':children}
-
-    def complement(self, children):
-        return {'type':'complement', **reduce(lambda a,b: {**a, **b}, children)}
-
-    def cmd(self, children):
-        return {'cmd': True}
-
-    def relative(self, children):
-        return {'type':'relative', **reduce(lambda a,b: {**a, **b}, children)}
-
-    def subject(self, children):
-        return {'subject': children}
-
-    def object(self, children):
-        return {'object': children}
+    def noun_cmd(self, _):
+        return {'cmd': Int(1)}
 
     def verb(self, children):
-        return {'verb': children}
+        return {'verb': children[0]}
+    
+    def noun_head(self, children):
+        return {'head':children[0]}
+
+    def relative_head(self, children):
+        return {'head':children[0]}
+
+    def subordinate(self, children):
+        return {'which':children[0]}
+
+    def noun(self, children):
+        return Implicit(**reduce(lambda a,b: {**a, **b}, children))
+    
+    def relative(self, children):
+        d=reduce(lambda a,b: {**a, **b}, children)
+        # add subject and object if absent??
+        return d['head'].copy(which=d['which'].copy(cmd=Int(0)))
+
+    def complement_head(self, children):
+        return {'complement_head':children[0]}
+
+    def preposition(self, children):
+        return {'preposition':children[0]}
+
+    def complement(self, children):
+        d=reduce(lambda a,b: {**a, **b}, children)
+        preposition= 'as_' if d['preposition']=='as' else d['preposition']
+        head=d['complement_head']
+        return {preposition:head}
+
+    def subject(self, children):
+        return {'subject': children[0]}
+
+    def object(self, children):
+        return {'object': children[0]}
 
     def simple_sentence(self, children):
-        print(children)
-        return {'type':'simple_sentence', **reduce(lambda a,b: {**a, **b}, children)}
+        d=reduce(lambda a,b: {**a, **b}, children)
+        return SimpleSentence(**d)
+
+    def left(self, children):
+        return {'left':children[0]}
+
+    def right(self, children):
+        return {'right':children[0]}
+
+    def op(self, children):
+        return {'op':children[0]}
 
     def compound_sentence(self, children):
-        pass
+        d=reduce(lambda a,b: {**a, **b}, children)
 
+        match d['op']:
+            case 'when':
+                return Def(definendum=d['left'], definition=d['right'])
+            case 'after':
+                return Law(effect=d['left'], cause=d['right'])
+            case _:
+                return BinExp(**d)
