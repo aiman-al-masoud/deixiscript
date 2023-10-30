3be18b40096c2d56079676c5d759859d4f94459d
aiman-al-masoud <aiman.almasoud01@universitadipavia.it>
2023-10-30 20:00:35 +0100

    ...

--- docs/thesis/build.sh
+++ docs/thesis/build.sh
@@ -1,7 +1,7 @@
 NAME='0-book'
 clear
 rm *.aux *.pdf *.log *.blg *.bbl
-# pdflatex "$NAME"
-# bibtex "$NAME"
-# pdflatex "$NAME"
-# pdflatex "$NAME"
+pdflatex "$NAME"
+bibtex "$NAME"
+pdflatex "$NAME"
+pdflatex "$NAME"
