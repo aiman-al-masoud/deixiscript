20fd2b906edd6ff40dd6de6efa1f6fddbb1aea98
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-11-06 16:14:24 +0100

    ...

--- src/plot/tests.py
+++ src/plot/tests.py
@@ -8,7 +8,7 @@ def tezt_c41():
     kb2 = the('man').does('ride').on(the('horse')).tell(kb1)
     kb3 = the('horse').does('move').tell(kb2)
     kb4 = the('man').does('yell').tell(kb3)
-    show(kb4.wm)
+    show(kb4)
     # save_png(kb4.wm)
  
 # tezt_c41()
