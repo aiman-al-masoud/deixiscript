a3ae4c834f64b3194535147ad3f06477e3fed55e
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-11-20 23:34:05 +0100

    ...

--- docs/thesis/figures/run.sh
+++ docs/thesis/figures/run.sh
@@ -1,2 +1,10 @@
 # dot -Tpng graph2.dot -Kdot  > output.png
-dot -Tpng "$1" -Kdot  > "$1.png"
+
+# dot -Tpng -Gsize=5,4\! "$1" -Kdot  > "$1.png"
+dot -Tpng  "$1" -Kdot  > "$1.png"
+
+# images=$(ls | grep dot)
+# for i in "$images"
+# do
+#     dot -Tpng -Gsize=5,4\! "$i" -Kdot  > "$i.png"
+# done
